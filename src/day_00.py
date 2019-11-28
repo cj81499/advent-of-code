fdff253247f2239825b427db9f4992e488200890
Cal Jacobson <cj81499@users.noreply.github.com>
2019-11-28 02:28:05 -0500

    initial commit
    setup repository

---
+++ src/day_00.py
@@ -0,0 +1,19 @@
+def part1(s, l):
+    pass
+
+
+def part2(s, l):
+    pass
+
+
+def main():
+    from datetime import date
+    import helpers
+    input_str, input_lines = helpers.get_puzzle(date(2019, 12, 0), )
+
+    print(f"part1: {part1(input_str, input_lines)}")
+    print(f"part2: {part2(input_str, input_lines)}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ test/test_00.py
@@ -0,0 +1,5 @@
+from src import day_00
+
+
+def test_day00_1():
+    assert day_00.part1("", []) is None
