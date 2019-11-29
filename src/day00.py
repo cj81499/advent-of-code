c9ed8e09e7dab17c79a4dd4f26c48806e9c0a642
Cal Jacobson <cj81499@users.noreply.github.com>
2019-11-29 01:27:28 -0500

    update template

--- src/day00.py
+++ src/day00.py
@@ -1,21 +1,22 @@
 from datetime import date
+from typing import List
 
 import helpers
 
 
-def part1(lines: list):
+def part1(text: str, lines: List[str]):
     pass
 
 
-def part2(lines: list):
+def part2(text: str, lines: List[str]):
     pass
 
 
 def main():
     input_txt, input_lines = helpers.get_puzzle(date(2015, 12, 0), )  # noqa
 
-    print(f"part1: {part1(input_lines)}")
-    print(f"part2: {part2(input_lines)}")
+    print(f"part1: {part1(input_txt, input_lines)}")
+    print(f"part2: {part2(input_txt, input_lines)}")
 
 
 if __name__ == "__main__":
--- test/day00_test.py
+++ test/day00_test.py
@@ -6,10 +6,10 @@ import day00
 
 class TestDay00(unittest.TestCase):
     def test_day00_part1(self):
-        self.assertEqual(None, day00.part1([]))
+        self.assertEqual(None, day00.part1("", []))
 
     def test_day00_part2(self):
-        self.assertEqual(None, day00.part2([]))
+        self.assertEqual(None, day00.part2("", []))
 
 
 if __name__ == "__main__":
