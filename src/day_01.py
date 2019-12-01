a56b0baca48e326921594579872b921f671ff29a
Cal Jacobson <cj81499@users.noreply.github.com>
2019-12-01 00:27:47 -0500

    day01

---
+++ src/day_01.py
@@ -0,0 +1,33 @@
+from datetime import date
+from typing import List
+
+import helpers
+
+
+def fuel_req(mass: int) -> int:
+    return mass // 3 - 2
+
+
+def fuel_req_rec(mass: int) -> int:
+    r = fuel_req(mass)
+    return 0 if 0 >= r else r + fuel_req_rec(r)
+
+
+def part1(nums: List[int]):
+    return sum(fuel_req(x) for x in nums)
+
+
+def part2(nums: List[int]):
+    return sum(fuel_req_rec(x) for x in nums)
+
+
+def main():
+    _, lines = helpers.get_puzzle(date(2019, 12, 1), "The Tyranny of the Rocket Equation")  # noqa
+    nums = [int(x) for x in lines]
+
+    print(f"part1: {part1(nums)}")
+    print(f"part2: {part2(nums)}")
+
+
+if __name__ == "__main__":
+    main()
