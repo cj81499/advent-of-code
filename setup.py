2612f81be4897a3a8c7f59b2bf3d76bf9400e9fd
Cal Jacobson <cj81499@users.noreply.github.com>
2020-01-31 11:40:17 -0500

    change package name
    remove python_version from mypy.ini

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name="AOC-2019", packages=find_packages())
+setup(
+    name="advent2019",
+    packages=find_packages(
+        exclude=("test", "*test*")
+    ),
+)
