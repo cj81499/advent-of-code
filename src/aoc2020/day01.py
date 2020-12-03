993859a10661f3df5f810da6062b3c9e484e1a92
Cal Jacobson <cj81499@users.noreply.github.com>
2020-12-02 23:42:26 -0500

    y2020d01ab

---
+++ src/aoc2020/day01.py
@@ -0,0 +1,27 @@
+def parta(txt):
+    nums = set(int(l) for l in txt.splitlines())
+
+    for n in nums:
+        remaining = 2020 - n
+        if remaining in nums:
+            return n * remaining
+
+
+def partb(txt):
+    nums = set(int(l) for l in txt.splitlines())
+
+    for x in nums:
+        for y in nums:
+            remaining = 2020 - (x + y)
+            if remaining in nums:
+                return x * y * remaining
+
+
+def main(txt):
+    print(f"parta: {parta(txt)}")
+    print(f"partb: {partb(txt)}")
+
+
+if __name__ == "__main__":
+    from aocd import data, submit
+    main(data)
