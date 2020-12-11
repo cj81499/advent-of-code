34a48c12a602b1056d172e75aae03aaf1ecb7d32
Cal Jacobson <cj81499@users.noreply.github.com>
2020-12-11 01:45:44 -0500

    y2020d11abt

---
+++ advent/aoc2020/day11.py
@@ -0,0 +1,84 @@
+import collections  # noqa
+import itertools  # noqa
+import re  # noqa
+
+FLOOR = "."
+EMPTY = "L"
+OCCUPIED = "#"
+
+
+def grid_get(grid, x, y):
+    if y < 0 or len(grid) <= y or x < 0 or len(grid[y]) <= x:
+        return None
+    return grid[y][x]
+
+
+def num_occupied_seats(fn):
+    def f(grid, x, y):
+        return fn(grid, x, y)[OCCUPIED]
+    return f
+
+
+def in_sight(fn):
+    return collections.Counter(
+        fn(dx, dy)
+        for dx in (-1, 0, 1)
+        for dy in (-1, 0, 1)
+        if (dx, dy) != (0, 0)
+    )
+
+
+def adj(grid, x, y):
+    return in_sight(lambda dx, dy: grid_get(grid, x + dx, y + dy))
+
+
+def visible(grid, x, y):
+    return in_sight(lambda dx, dy: seen_in_direction(grid, x, y, dx, dy))
+
+
+def seen_in_direction(grid, x, y, dx, dy):
+    i = 1
+    while (val := grid_get(grid, x + i * dx, y + i * dy)) == FLOOR:
+        i += 1
+    return val
+
+
+def solver(txt, fn, max_people):
+    grid = txt.splitlines()
+    last_grid_str = None
+    grid_str = txt
+    while grid_str != last_grid_str:
+        next_grid = []
+        for y, row in enumerate(grid):
+            next_row = []
+            for x, entry in enumerate(row):
+                next_entry = entry
+                if entry == EMPTY:
+                    next_entry = OCCUPIED if fn(grid, x, y) == 0 else EMPTY
+                elif entry == OCCUPIED:
+                    next_entry = EMPTY if fn(grid, x, y) >= max_people else OCCUPIED
+                next_row.append(next_entry)
+            next_grid.append("".join(str(x) for x in next_row))
+        last_grid_str = grid_str
+        grid = next_grid
+        grid_str = "\n".join(grid)
+
+    return grid_str.count(OCCUPIED)
+
+
+def parta(txt):
+    return solver(txt, num_occupied_seats(adj), 4)
+
+
+def partb(txt):
+    return solver(txt, num_occupied_seats(visible), 5)
+
+
+def main(txt):
+    print(f"parta: {parta(txt)}")
+    print(f"partb: {partb(txt)}")
+
+
+if __name__ == "__main__":
+    from aocd import data
+    main(data)
---
+++ tests/aoc2020/y2020d11_test.py
@@ -0,0 +1,22 @@
+import advent.aoc2020.day11 as d
+
+EXAMPLE_INPUT = """
+L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL
+""".strip()
+
+
+def test_a():
+    assert d.parta(EXAMPLE_INPUT) == 37
+
+
+def test_b():
+    assert d.partb(EXAMPLE_INPUT) == 26
