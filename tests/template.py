0a4d9e1bc28a0684498a75d7750a0bd58c42cc3e
Cal Jacobson <cj81499@users.noreply.github.com>
2020-12-12 01:43:49 -0500

    add test template

---
+++ tests/template.py
@@ -0,0 +1,12 @@
+import advent.aoc2020.dayXX as d
+
+EXAMPLE_INPUT = """
+""".strip()
+
+
+def test_a():
+    assert d.parta(EXAMPLE_INPUT) == -1
+
+
+def test_b():
+    assert d.partb(EXAMPLE_INPUT) == -1
