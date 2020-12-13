2d4566109bce9ff39bf8be22f152fc6a57be52ce
Cal Jacobson <cj81499@users.noreply.github.com>
2020-12-13 15:02:47 -0500

    y2020d13abt

---
+++ advent/aoc2020/day13.py
@@ -0,0 +1,57 @@
+import collections  # noqa
+import itertools  # noqa
+import math
+import re  # noqa
+
+
+def parta(txt):
+    arrival_time, bus_ids = txt.splitlines()
+    arrival_time = int(arrival_time)
+    bus_ids = bus_ids.split(",")
+    bus_ids = [int(x) for x in bus_ids if x != "x"]
+    earliest = (float("inf"), None)
+    for bus_id in bus_ids:
+        bus_departure_time = arrival_time - (arrival_time % bus_id) + bus_id
+        if bus_departure_time < earliest[0]:
+            earliest = (bus_departure_time, bus_id)
+    earliest_bus = earliest[1]
+    wait_time = earliest[0] - arrival_time
+    return earliest_bus * wait_time
+
+
+def bus_leaves_at(bus_id, t):
+    if t == 0:
+        return True
+    return t % bus_id == 0
+
+
+def lcm(nums):
+    first, *rest = nums
+    if len(rest) == 0:
+        return first
+    l = lcm(rest)
+    return (first * l) // math.gcd(first, l)
+
+
+def partb(txt):
+    _, bus_ids = txt.splitlines()
+    bus_ids = bus_ids.split(",")
+
+    t = 0
+    for i, bus_id in enumerate(bus_ids):
+        if i == 0 or bus_id == "x":
+            continue
+        step_size = lcm(int(id) for id in bus_ids[:i] if id != "x")
+        while not bus_leaves_at(int(bus_id), t + i):
+            t += step_size
+    return t
+
+
+def main(txt):
+    print(f"parta: {parta(txt)}")
+    print(f"partb: {partb(txt)}")
+
+
+if __name__ == "__main__":
+    from aocd import data
+    main(data)
---
+++ tests/aoc2020/y2020d13_test.py
@@ -0,0 +1,23 @@
+import advent.aoc2020.day13 as d
+import pytest
+
+EXAMPLE_INPUT = """
+939
+7,13,x,x,59,x,31,19
+""".strip()
+
+
+def test_a():
+    assert d.parta(EXAMPLE_INPUT) == 295
+
+
+@pytest.mark.parametrize("input, expected", [
+    ("0\n17,x,13,19", 3417),
+    ("0\n67,7,59,61", 754018),
+    ("0\n67,x,7,59,61", 779210),
+    ("0\n67,7,x,59,61", 1261476),
+    ("0\n1789,37,47,1889", 1202161486),
+    (EXAMPLE_INPUT, 1068781),
+])
+def test_b(input, expected):
+    assert d.partb(input) == expected
