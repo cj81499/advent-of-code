57fc7665aa41f44e9178e2c372caf3aaa82870f1
Cal Jacobson <cj81499@users.noreply.github.com>
2020-12-14 01:08:05 -0500

    cleanup

--- advent/aoc2020/day14.py
+++ advent/aoc2020/day14.py
@@ -3,22 +3,15 @@ import itertools  # noqa
 import re  # noqa
 
 
-def apply_mask(mask, binary, v2=False):
-    masked = []
-    for m, b in zip(mask, binary):
-        if not v2:
-            masked.append(b if m == "X" else m)
-        else:
-            if m == "0":
-                masked.append(b)
-            elif m == "1":
-                masked.append("1")
-            else:
-                masked.append("X")
-    return "".join(masked)
-
-
 def parta(txt):
+    return solver(txt, lambda m, a: (a,), lambda m, v: int(apply_mask(m, bin36(v)), 2))
+
+
+def partb(txt):
+    return solver(txt, lambda m, a: (addresses(apply_mask(m, bin36(a), v2=True))), lambda m, v: v)
+
+
+def solver(txt, addrs_fn, val_fn):
     mem = {}
     mask = 0
     for instruction in txt.splitlines():
@@ -27,11 +20,22 @@ def parta(txt):
             mask = rhs
         else:
             addr = int(lhs[4:-1])
-            val = int(rhs)
-            binary = str(bin(val))[2:].zfill(36)
-            result = apply_mask(mask, binary)
-            mem[addr] = result
-    return sum(int(x, 2) for x in mem.values())
+            val = val_fn(mask, int(rhs))
+            for a in addrs_fn(mask, addr):
+                mem[a] = val
+    return sum(mem.values())
+
+
+def apply_mask(mask, binary, v2=False):
+    masked = []
+    append_switch = "0" if v2 else "X"
+    for m, b in zip(mask, binary):
+        masked.append(b if m == append_switch else m)
+    return "".join(masked)
+
+
+def bin36(n):
+    return str(bin(n))[2:].zfill(36)
 
 
 def addresses(addr):
@@ -47,23 +51,6 @@ def addresses(addr):
         addrs.append(prefix + "0" + sub)
         addrs.append(prefix + "1" + sub)
     return addrs
-
-
-def partb(txt):
-    mem = {}
-    mask = 0
-    for instruction in txt.splitlines():
-        lhs, rhs = instruction.split(" = ")
-        if lhs == "mask":
-            mask = rhs
-        else:
-            addr = int(lhs[4:-1])
-            val = int(rhs)
-            new_addr = apply_mask(mask, str(bin(addr))[2:].zfill(36), v2=True)
-            addrs = addresses(new_addr)
-            for a in addrs:
-                mem[a] = val
-    return sum(mem.values())
 
 
 def main(txt):
