4320757a4e2bca2651b7bdc9d5c109f958f3d035
Cal Jacobson <cj81499@users.noreply.github.com>
2020-12-23 23:10:17 -0500

    don't submit garbage answers via `aoc` runner

--- advent/__init__.py
+++ advent/__init__.py
@@ -4,11 +4,13 @@ from pathlib import Path
 
 
 def solve(year, day, data):
-    ans_a, ans_b = "unknown", "unknown"
+    ans_a, ans_b = None, None
     try:
         pkg = importlib.import_module(f"advent.aoc{year}.day{day:02d}")
-        ans_a = pkg.parta(data) if hasattr(pkg, "parta") else "no parta"
-        ans_b = pkg.partb(data) if hasattr(pkg, "partb") else "no partb"
+        if hasattr(pkg, "parta"):
+            ans_a = pkg.parta(data)
+        if hasattr(pkg, "partb"):
+            ans_b = pkg.partb(data)
         return ans_a, ans_b
     except Exception as e:
         p = Path(__file__).parent / f"aoc{year}/day{day:02d}.log"
