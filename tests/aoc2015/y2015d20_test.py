e74daeb27ed589a57298078269ce50611806c91f
Cal Jacobson <cj81499@users.noreply.github.com>
2021-01-03 02:33:47 -0500

    y2015d20abt

---
+++ tests/aoc2015/y2015d20_test.py
@@ -0,0 +1,15 @@
+import advent.aoc2015.day20 as d
+
+
+def test_a():
+    # 1000 is for large enough to generate the first 10 houses
+    presents = d.presents_for_houses(1000, 10)
+    assert presents[1] == 10
+    assert presents[2] == 30
+    assert presents[3] == 40
+    assert presents[4] == 70
+    assert presents[5] == 60
+    assert presents[6] == 120
+    assert presents[7] == 80
+    assert presents[8] == 150
+    assert presents[9] == 130
