0a497c3c50b7fa2b41349acd341d22c43b4b611d
Cal Jacobson <cj81499@users.noreply.github.com>
2021-01-04 23:11:44 -0500

    y2015d24abt

---
+++ tests/aoc2015/y2015d24_test.py
@@ -0,0 +1,11 @@
+import advent.aoc2015.day24 as d
+
+EXAMPLE_INPUT = "1 2 3 4 5 7 8 9 10 11".replace(" ", "\n")
+
+
+def test_a():
+    assert d.parta(EXAMPLE_INPUT) == 99
+
+
+def test_b():
+    assert d.partb(EXAMPLE_INPUT) == 44
