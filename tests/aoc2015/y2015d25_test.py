2ce7bd312395a385761aa1924d5bb069da6c5fea
Cal Jacobson <cj81499@users.noreply.github.com>
2021-01-05 21:03:29 -0500

    y2015d25abt

---
+++ tests/aoc2015/y2015d25_test.py
@@ -0,0 +1,20 @@
+import advent.aoc2015.day25 as d
+
+
+def test_next_code():
+    assert d.next_code(d.FIRST_CODE) == 31916031
+
+
+def test_code_at():
+    assert d.code_at(1, 1) == d.FIRST_CODE
+    assert d.code_at(1, 2) == 31916031
+    assert d.code_at(1, 3) == 16080970
+    assert d.code_at(1, 4) == 24592653
+
+
+# def test_a():
+#     assert d.parta(EXAMPLE_INPUT) == 99
+
+
+# def test_b():
+#     assert d.partb(EXAMPLE_INPUT) == 44
