9e01698e61738cdc08b1df72dc17a0086d55d2b9
Cal Jacobson <cj81499@users.noreply.github.com>
2021-01-08 23:37:48 -0500

    remove unrelated comment
    (copy/paste error)

--- tests/aoc2017/y2017d05_test.py
+++ tests/aoc2017/y2017d05_test.py
@@ -2,12 +2,6 @@ import advent.aoc2017.day05 as d
 
 EXAMPLE_INPUT_0 = "0 3 0 1 -3".replace(" ", "\n")
 
-# EXAMPLE_INPUT_1 = """
-# 5 9 2 8
-# 9 4 7 3
-# 3 8 6 5
-# """.strip()
-
 
 def test_a():
     assert d.parta(EXAMPLE_INPUT_0) == 5
