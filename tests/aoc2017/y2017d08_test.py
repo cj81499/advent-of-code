3777d8d855d002a8254fbd7d79c823592768a429
Cal Jacobson <cj81499@users.noreply.github.com>
2021-01-10 16:24:48 -0500

    y2017d08abt

---
+++ tests/aoc2017/y2017d08_test.py
@@ -0,0 +1,16 @@
+import advent.aoc2017.day08 as d
+
+EXAMPLE_INPUT = """
+b inc 5 if a > 1
+a inc 1 if b < 5
+c dec -10 if a >= 1
+c inc -20 if c == 10
+""".strip()
+
+
+def test_a():
+    assert d.parta(EXAMPLE_INPUT) == 1
+
+
+def test_b():
+    assert d.partb(EXAMPLE_INPUT) == 10
