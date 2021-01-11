784f8b5e86f2f021c8bc52566bcd52e24f34c77f
Cal Jacobson <cj81499@users.noreply.github.com>
2021-01-10 23:19:56 -0500

    y2017d10abt

---
+++ tests/aoc2017/y2017d10_test.py
@@ -0,0 +1,20 @@
+import pytest
+
+import advent.aoc2017.day10 as d
+
+EXAMPLE_INPUT = """
+""".strip()
+
+
+def test_a():
+    assert d.parta("3, 4, 1, 5", list_size=5) == 12
+
+
+@pytest.mark.parametrize("input, expected", [
+    ("", "a2582a3a0e66e6e86e3812dcb672a272"),
+    ("AoC 2017", "33efeb34ea91902bb2f59c9920caa6cd"),
+    ("1,2,3", "3efbe78a8d82f29979031a4aa0b16a9d"),
+    ("1,2,4", "63960835bcdc130f0b66d7ff4f6a5a8e"),
+])
+def test_b(input, expected):
+    assert d.partb(input) == expected
