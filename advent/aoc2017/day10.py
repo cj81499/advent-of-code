5fddfe5217744dac03b343e175d1b3ae9929f67e
Cal Jacobson <cj81499@users.noreply.github.com>
2021-01-12 14:43:02 -0500

    y2017d14abt
    includes refactor to y2017d10b

--- advent/aoc2017/day10.py
+++ advent/aoc2017/day10.py
@@ -31,14 +31,7 @@ def dense_hash(sparse_hash):
     return "".join(hex(reduce(xor, c))[2:].zfill(2) for c in chunks(sparse_hash, 16))
 
 
-def parta(txt: str, list_size: int = DEFAULT_LIST_SIZE):
-    lengths = list(map(int, txt.split(",")))
-    nums = [*range(list_size)]
-    do_round(nums, lengths)
-    return nums[0] * nums[1]
-
-
-def partb(txt: str):
+def knot_hash(txt: str):
     ascii_codes = list(map(ord, txt))
     lengths = [*ascii_codes, *SUFFIX]
     nums = [*range(DEFAULT_LIST_SIZE)]
@@ -48,6 +41,17 @@ def partb(txt: str):
         pos, skip_size = do_round(nums, lengths, pos=pos, skip_size=skip_size)
     sparse_hash = tuple(nums)
     return dense_hash(sparse_hash)
+
+
+def parta(txt: str, list_size: int = DEFAULT_LIST_SIZE):
+    lengths = list(map(int, txt.split(",")))
+    nums = [*range(list_size)]
+    do_round(nums, lengths)
+    return nums[0] * nums[1]
+
+
+def partb(txt: str):
+    return knot_hash(txt)
 
 
 def main(txt: str):
---
+++ tests/aoc2017/y2017d14_test.py
@@ -0,0 +1,11 @@
+import advent.aoc2017.day14 as d
+
+EXAMPLE_INPUT = "flqrgnkx"
+
+
+def test_a():
+    assert d.parta(EXAMPLE_INPUT) == 8108
+
+
+def test_b():
+    assert d.partb(EXAMPLE_INPUT) == 1242
