c1ae32c817c9ae60a4c9f60f709c8ad708d6d4fd
Cal Jacobson <cj81499@users.noreply.github.com>
2021-01-12 11:53:42 -0500

    y2017d13abt

---
+++ tests/aoc2017/y2017d13_test.py
@@ -0,0 +1,16 @@
+import advent.aoc2017.day13 as d
+
+EXAMPLE_INPUT = """
+0: 3
+1: 2
+4: 4
+6: 4
+""".strip()
+
+
+def test_a():
+    assert d.parta(EXAMPLE_INPUT) == 24
+
+
+def test_b():
+    assert d.partb(EXAMPLE_INPUT) == 10
