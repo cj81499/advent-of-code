a80ac8aa11b5daf533bc3872d24cb878df89ddaa
Cal Jacobson <cj81499@users.noreply.github.com>
2021-01-13 14:48:51 -0500

    y2017d17abt

---
+++ advent/aoc2017/day17.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import itertools  # noqa
+import re  # noqa
+from collections import Counter, defaultdict, deque  # noqa
+
+TWO_THOUSAND_SEVENTEEN = 2017
+
+
+def parta(txt: str, element_after=TWO_THOUSAND_SEVENTEEN, loops=TWO_THOUSAND_SEVENTEEN):
+    n = int(txt)
+    d = deque([0])
+    for i in range(loops):
+        d.rotate(-n-1)
+        d.appendleft(i + 1)
+    return d[(d.index(element_after) + 1) % len(d)]
+
+
+def partb(txt: str):
+    return parta(txt, element_after=0, loops=50000000)
+
+
+def main(txt: str):
+    print(f"parta: {parta(txt)}")
+    print(f"partb: {partb(txt)}")
+
+
+if __name__ == "__main__":
+    from aocd import data
+    main(data)
