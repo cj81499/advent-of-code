b1b32eaaa7bd376d87f720f8cb87a41018a63c23
Cal Jacobson <cj81499@users.noreply.github.com>
2021-01-13 01:25:34 -0500

    y2017d15abt

---
+++ tests/aoc2017/y2017d15_test.py
@@ -0,0 +1,36 @@
+import pytest
+
+import advent.aoc2017.day15 as d
+
+EXAMPLE_INPUT = """
+Generator A starts with 65
+Generator B starts with 8921
+""".strip()
+
+
+@pytest.mark.parametrize("num_loops, expected", [
+    (2, 0),
+    (3, 1),
+    (5, 1),
+])
+def test_a(num_loops, expected):
+    assert d.parta(EXAMPLE_INPUT, num_loops=num_loops) == expected
+
+
+@pytest.mark.slow
+def test_a_slow():
+    assert d.parta(EXAMPLE_INPUT) == 588
+
+
+@pytest.mark.parametrize("num_loops, expected", [
+    (5, 0),
+    (1055, 0),
+    (1056, 1),
+])
+def test_b(num_loops, expected):
+    assert d.partb(EXAMPLE_INPUT, num_loops=num_loops) == expected
+
+
+@pytest.mark.slow
+def test_b_slow():
+    assert d.partb(EXAMPLE_INPUT) == 309
