cd73135942e08a15f1ab5ecd33b909a83bbbcc4b
Cal Jacobson <cj81499@users.noreply.github.com>
2021-01-13 19:31:46 -0500

    y2017d18abt

---
+++ tests/aoc2017/y2017d18_test.py
@@ -0,0 +1,32 @@
+import advent.aoc2017.day18 as d
+
+EXAMPLE_INPUT_0 = """
+set a 1
+add a 2
+mul a a
+mod a 5
+snd a
+set a 0
+rcv a
+jgz a -1
+set a 1
+jgz a -2
+""".strip()
+
+EXAMPLE_INPUT_1 = """
+snd 1
+snd 2
+snd p
+rcv a
+rcv b
+rcv c
+rcv d
+""".strip()
+
+
+def test_a():
+    assert d.parta(EXAMPLE_INPUT_0) == 4
+
+
+def test_b():
+    assert d.partb(EXAMPLE_INPUT_1) == 3
