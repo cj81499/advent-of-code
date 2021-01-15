526785ec7731b996c110b3d2cdfcdad2c2d8fecb
Cal Jacobson <cj81499@users.noreply.github.com>
2021-01-15 00:00:47 -0500

    y2017d19abt

---
+++ tests/aoc2017/y2017d19_test.py
@@ -0,0 +1,17 @@
+import advent.aoc2017.day19 as d
+
+EXAMPLE_INPUT = \
+    "    |         \n" \
+    "    |  +--+   \n" \
+    "    A  |  C   \n" \
+    "F---|----E|--+\n" \
+    "    |  |  |  D\n" \
+    "    +B-+  +--+"
+
+
+def test_a():
+    assert d.parta(EXAMPLE_INPUT) == "ABCDEF"
+
+
+def test_b():
+    assert d.partb(EXAMPLE_INPUT) == 38
