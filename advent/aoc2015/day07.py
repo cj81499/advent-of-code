cb01573c151e4768054d312ae53cacec65b43164
Cal Jacobson <cj81499@users.noreply.github.com>
2021-01-17 21:45:28 -0500

    redo y2015d07abt

--- advent/aoc2015/day07.py
+++ advent/aoc2015/day07.py
@@ -1,117 +1,50 @@
-from collections import deque
-from typing import Deque
+from __future__ import annotations
+
+from functools import cache
+from operator import and_, lshift, or_, rshift
 
 
-def is_int(s: str):
-    try:
-        int(s)
-        return True
-    except:  # noqa
-        return False
+class HashableDict(dict):
+    def __hash__(self):
+        return hash(tuple(sorted(self.items())))
 
 
-class Instruction():
-    BIT_MASK = 0b1111111111111111
-
-    def __init__(self, instruction_str: str):
-        self._instruction_str = instruction_str
-        source, self.destination = instruction_str.split(" -> ")
-        self.source_info = source.split()
-
-    def can_execute(self, wires: dict):
-        if len(self.source_info) == 1:
-            return is_int(self.source_info[0]) or self.source_info[0] in wires
-
-        elif len(self.source_info) == 2:
-            _, source = self.source_info
-            return source in wires
-
-        elif len(self.source_info) == 3:
-            source_1, _, source_2 = self.source_info
-
-            source_1_good = is_int(source_1) or source_1 in wires
-            source_2_good = is_int(source_2) or source_2 in wires
-
-            return source_1_good and source_2_good
-
-        return False
-
-    def execute(self, wires):  # noqa
-        assert self.can_execute(wires)
-        if len(self.source_info) == 1:
-            if is_int(self.source_info[0]):
-                wires[self.destination] = int(
-                    self.source_info[0]) & Instruction.BIT_MASK
-            else:
-                wires[self.destination] = wires[self.source_info[0]
-                                                ] & Instruction.BIT_MASK
-        elif len(self.source_info) == 2:
-            mode, source = self.source_info
-            if not is_int(source):
-                source = wires[source]
-            if mode == "NOT":
-                wires[self.destination] = ~ int(source) & Instruction.BIT_MASK
-            else:
-                print(mode)
-
-        elif len(self.source_info) == 3:
-            source_1, mode, source_2 = self.source_info
-            if not is_int(source_1):
-                source_1 = wires[source_1]
-            if not is_int(source_2):
-                source_2 = wires[source_2]
-            if mode == "RSHIFT":
-                wires[self.destination] = int(source_1) >> int(
-                    source_2) & Instruction.BIT_MASK
-            elif mode == "LSHIFT":
-                wires[self.destination] = int(source_1) << int(
-                    source_2) & Instruction.BIT_MASK
-            elif mode == "AND":
-                wires[self.destination] = int(source_1) & int(
-                    source_2) & Instruction.BIT_MASK
-            elif mode == "OR":
-                wires[self.destination] = int(source_1) | int(
-                    source_2) & Instruction.BIT_MASK
-            else:
-                print(mode)
-
-    def __repr__(self):
-        return self._instruction_str
+@cache
+def signal_on(wire, wires):
+    if wire.isnumeric():
+        return int(wire)
+    val = wires[wire]
+    if val.isnumeric():
+        return int(val)
+    if val.isalpha():
+        return signal_on(val, wires)
+    if "NOT" in val:
+        return ~signal_on(val.lstrip("NOT "), wires) & 0xFFFF
+    lhs, op, rhs = val.split()
+    return {
+        "LSHIFT": lshift,
+        "RSHIFT": rshift,
+        "AND": and_,
+        "OR": or_,
+    }[op](signal_on(lhs, wires), signal_on(rhs, wires))
 
 
-def build_instructions_deque(txt, partb=False) -> Deque[Instruction]:
-    instructions: Deque[Instruction] = deque()
-
-    for instruction in txt.splitlines():
-        i = Instruction(instruction)
-        if not partb or i.destination != "b":
-            instructions.append(i)
-
-    return instructions
+def parse_wires(txt):
+    return dict(reversed(line.split(" -> ")) for line in txt.splitlines())
 
 
-def run(instructions: Deque[Instruction], wires: dict):
-    while instructions:
-        i: Instruction = instructions.popleft()
-        if i.can_execute(wires):
-            i.execute(wires)
-        else:
-            instructions.append(i)
+def parta(txt: str):
+    wires = HashableDict(parse_wires(txt))
+    return signal_on("a", wires)
 
 
-def parta(txt):
-    wires = {}
-    run(build_instructions_deque(txt), wires)
-    return wires["a"]
+def partb(txt: str):
+    wires = HashableDict(parse_wires(txt))
+    wires["b"] = str(signal_on("a", wires))
+    return signal_on("a", wires)
 
 
-def partb(txt):
-    wires = {"b": parta(txt)}
-    run(build_instructions_deque(txt, partb=True), wires)
-    return wires["a"]
-
-
-def main(txt):
+def main(txt: str):
     print(f"parta: {parta(txt)}")
     print(f"partb: {partb(txt)}")
 
