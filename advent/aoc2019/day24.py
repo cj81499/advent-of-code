10aca2698eb8f2fb040aa64408766f1a54d8e89a
Cal Jacobson <cj81499@users.noreply.github.com>
2021-01-31 12:08:56 -0500

    y2019d24abt

--- advent/aoc2019/day24.py
+++ advent/aoc2019/day24.py
@@ -1,77 +1,102 @@
-BUG = "#"
+from __future__ import annotations
 
-GRID_SIZE = 5
+import itertools
+from typing import FrozenSet, Tuple
 
-Bugs = set[complex]
-State = tuple[tuple[int, int], ...]
+Bug = Tuple[int, ...]
+Bugs = FrozenSet[Bug]
 
 
-def adj(pos: complex) -> list[complex]:
-    adjustments = (1 + 0j, -1 + 0j, 0 + 1j, 0 - 1j)
-    return [pos + adjust for adjust in adjustments]
+def str_to_bugs(s: str, recursive=False) -> Bugs:
+    bugs = set(
+        (x, y, 0) if recursive else (x, y)
+        for y, line in enumerate(s.splitlines())
+        for x, c in enumerate(line)
+        if c == "#"
+    )
+    if recursive:
+        bugs.discard((2, 2, 0))
+    return frozenset(bugs)
+
+
+def parta(txt: str):
+    bugs: Bugs = str_to_bugs(txt)
+    repeat = first_repeat(bugs)
+    return biodiversity(repeat)
+
+
+def first_repeat(bugs: Bugs) -> Bugs:
+    seen = set()
+    while bugs not in seen:
+        seen.add(bugs)
+        bugs = step(bugs)
+    return bugs
+
+
+def should_add_bug(is_bug, adj_count):
+    return (is_bug and adj_count == 1) or (not is_bug and adj_count in (1, 2))
 
 
 def step(bugs: Bugs) -> Bugs:
     new_bugs = set()
-    for y in range(GRID_SIZE):
-        for x in range(GRID_SIZE):
-            pos = complex(x, y)
-            adj_count = sum(1 for p in adj(pos) if p in bugs)
-            if (adj_count == 1 if pos in bugs else adj_count in (1, 2)):
-                new_bugs.add(pos)
-    return new_bugs
-
-
-def parse_bugs(lines: list[str]) -> Bugs:
-    bugs = set()
-    for y, line in enumerate(lines):
-        for x, c in enumerate(line):
-            if c == BUG:
-                bugs.add(complex(x, y))
-    return bugs
-
-
-def bugs_to_state(bugs: Bugs) -> State:
-    return tuple(sorted((int(c.real), int(c.imag)) for c in bugs))
-
-
-def find_first_repeat(bugs: Bugs) -> Bugs:
-    seen: set[State] = set()
-    state = bugs_to_state(bugs)
-    while state not in seen:
-        seen.add(state)
-        bugs = step(bugs)
-        state = bugs_to_state(bugs)
-    return bugs
+    for x, y in itertools.product(range(5), repeat=2):
+        p = (x, y)
+        is_bug = p in bugs
+        adj_count = sum(1 for dx, dy in ((0, -1), (1, 0), (0, 1), (-1, 0)) if (x + dx, y + dy) in bugs)
+        if should_add_bug(is_bug, adj_count):
+            new_bugs.add(p)
+    return frozenset(new_bugs)
 
 
 def biodiversity(bugs: Bugs) -> int:
-    total_points = 0
-    point_at_pos = 1
-    for y in range(GRID_SIZE):
-        for x in range(GRID_SIZE):
-            pos = complex(x, y)
-            if pos in bugs:
-                total_points += point_at_pos
-            point_at_pos *= 2
-    return total_points
+    return sum(2 ** (5 * y + x) for x, y in bugs)
 
 
-def parta(txt: str) -> int:
-    lines = txt.splitlines()
-
-    bugs = parse_bugs(lines)
-    first_repeat = find_first_repeat(bugs)
-    return biodiversity(first_repeat)
-
-
-# def partb(lines: list[str]) -> int:
-#     return 0
+def partb(txt: str, minutes=200):
+    print(txt)
+    bugs: Bugs = str_to_bugs(txt, recursive=True)
+    for _ in range(minutes):
+        bugs = step_recursive(bugs)
+    return len(bugs)
 
 
-def main(txt) -> None:
+def adj_count_recursive(bugs, pos: Bug):
+    x, y, z = pos
+    adj_count = sum(1 for dx, dy in ((0, -1), (1, 0), (0, 1), (-1, 0)) if (x + dx, y + dy, z) in bugs)
+
+    # check outer edge of layer
+    adj_count += 1 if x == 0 and (1, 2, z - 1) in bugs else 0
+    adj_count += 1 if x == 4 and (3, 2, z - 1) in bugs else 0
+    adj_count += 1 if y == 0 and (2, 1, z - 1) in bugs else 0
+    adj_count += 1 if y == 4 and (2, 3, z - 1) in bugs else 0
+
+    # check inside
+    adj_count += 0 if (x, y) != (2, 1) else sum(1 for inner_x in range(5) if (inner_x, 0, z + 1) in bugs)
+    adj_count += 0 if (x, y) != (2, 3) else sum(1 for inner_x in range(5) if (inner_x, 4, z + 1) in bugs)
+    adj_count += 0 if (x, y) != (1, 2) else sum(1 for inner_y in range(5) if (0, inner_y, z + 1) in bugs)
+    adj_count += 0 if (x, y) != (3, 2) else sum(1 for inner_y in range(5) if (4, inner_y, z + 1) in bugs)
+
+    return adj_count
+
+
+def step_recursive(bugs: Bugs) -> Bugs:
+    new_bugs = set()
+    min_z = min(z for _x, _y, z in bugs)
+    max_z = max(z for _x, _y, z in bugs)
+    for z in range(min_z - 1, max_z + 2):
+        for x, y in itertools.product(range(5), repeat=2):
+            if (x, y) != (2, 2):  # the middle (2,2) can't be a bug. it is a recursive space.
+                p = (x, y, z)
+                is_bug = p in bugs
+                adj_count = adj_count_recursive(bugs, p)
+                if should_add_bug(is_bug, adj_count):
+                    new_bugs.add(p)
+    return frozenset(new_bugs)
+
+
+def main(txt: str):
     print(f"parta: {parta(txt)}")
-    # print(f"partb: {partb(lines)}")
+    print(f"partb: {partb(txt)}")
 
 
 if __name__ == "__main__":
--- tests/aoc2019/y2019d24_test.py
+++ tests/aoc2019/y2019d24_test.py
@@ -1,3 +1,7 @@
+import itertools
+
+import pytest
+
 import advent.aoc2019.day24 as d
 
 INITIAL = """
@@ -8,8 +12,7 @@ INITIAL = """
 #....
 """.strip()
 
-
-AFTER_1_MINS = """
+AFTER_1 = """
 #..#.
 ####.
 ###.#
@@ -17,7 +20,7 @@ AFTER_1_MINS = """
 .##..
 """.strip()
 
-AFTER_2_MINS = """
+AFTER_2 = """
 #####
 ....#
 ....#
@@ -25,7 +28,7 @@ AFTER_2_MINS = """
 #.###
 """.strip()
 
-AFTER_3_MINS = """
+AFTER_3 = """
 #....
 ####.
 ...##
@@ -33,7 +36,7 @@ AFTER_3_MINS = """
 .##.#
 """.strip()
 
-AFTER_4_MINS = """
+AFTER_4 = """
 ####.
 ....#
 ##..#
@@ -50,62 +53,45 @@ FIRST_REPEAT = """
 """.strip()
 
 
-def test_parse_bugs() -> None:
-    bugs = d.parse_bugs(INITIAL.splitlines())
-
-    assert 0 + 0j not in bugs
-    assert 1 + 0j not in bugs
-    assert 2 + 0j not in bugs
-    assert 3 + 0j not in bugs
-    assert 4 + 0j in bugs
-    assert 0 + 1j in bugs
-    assert 1 + 1j not in bugs
-    assert 2 + 1j not in bugs
-    assert 3 + 1j in bugs
-    assert 4 + 1j not in bugs
-    assert 0 + 2j in bugs
-    assert 1 + 2j not in bugs
-    assert 2 + 2j not in bugs
-    assert 3 + 2j in bugs
-    assert 4 + 2j in bugs
-    assert 0 + 3j not in bugs
-    assert 1 + 3j not in bugs
-    assert 2 + 3j in bugs
-    assert 3 + 3j not in bugs
-    assert 4 + 3j not in bugs
-    assert 0 + 4j in bugs
-    assert 1 + 4j not in bugs
-    assert 2 + 4j not in bugs
-    assert 3 + 4j not in bugs
-    assert 4 + 4j not in bugs
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
 
 
-def test_step() -> None:
-    initial = d.parse_bugs(INITIAL.splitlines())
-    one = d.step(initial)
-    assert one == d.parse_bugs(AFTER_1_MINS.splitlines())
-    two = d.step(one)
-    assert two == d.parse_bugs(AFTER_2_MINS.splitlines())
-    three = d.step(two)
-    assert three == d.parse_bugs(AFTER_3_MINS.splitlines())
-    four = d.step(three)
-    assert four == d.parse_bugs(AFTER_4_MINS.splitlines())
+def test_str_to_bugs():
+    assert d.str_to_bugs(INITIAL) == frozenset([
+        (4, 0),
+        (0, 1), (3, 1),
+        (0, 2), (3, 2), (4, 2),
+        (2, 3),
+        (0, 4),
+    ])
 
 
-def test_find_first_repeat() -> None:
-    bugs = d.parse_bugs(INITIAL.splitlines())
-    repeat = d.find_first_repeat(bugs)
-    assert repeat == d.parse_bugs(FIRST_REPEAT.splitlines())
+@pytest.mark.parametrize("before, after", pairwise([
+    INITIAL, AFTER_1, AFTER_2, AFTER_3, AFTER_4
+]))
+def test_step(before, after):
+    print(f"{before=}, {after=}")
+    before = d.str_to_bugs(before)
+    after = d.str_to_bugs(after)
+    print(f"{before=}, {after=}")
+    assert d.step(before) == after
 
 
-def test_biodiversity() -> None:
-    bugs = d.parse_bugs(FIRST_REPEAT.splitlines())
-    assert d.biodiversity(bugs) == 2129920
+def test_first_repeat():
+    assert d.first_repeat(d.str_to_bugs(INITIAL)) == d.str_to_bugs(FIRST_REPEAT)
 
 
-def test_parta() -> None:
+def test_biodiversity():
+    assert d.biodiversity(d.str_to_bugs(FIRST_REPEAT)) == 2129920
+
+
+def test_a():
     assert d.parta(INITIAL) == 2129920
 
 
-# def test_partb() -> None:
-#     assert d.partb([]) == 0
+def test_b():
+    assert d.partb(INITIAL, minutes=10) == 99
