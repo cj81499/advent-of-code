6314543adbbdd6511d075c5d8ec475af406fa3a2
Cal Jacobson <cj81499@users.noreply.github.com>
2021-02-03 00:55:22 -0500

    y2016d03abt

---
+++ tests/aoc2016/y2016d03_test.py
@@ -0,0 +1,11 @@
+import advent.aoc2016.day03 as d
+
+EXAMPLE_INPUT = "5 10 25"
+
+
+def test_is_valid_triangle():
+    assert d.is_valid_triangle(EXAMPLE_INPUT.split()) is False
+
+
+def test_a():
+    assert d.parta(EXAMPLE_INPUT) == 0
