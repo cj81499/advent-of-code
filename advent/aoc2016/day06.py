e87153b4aeb14c786d373c216899005a4987c1d2
Cal Jacobson <cj81499@users.noreply.github.com>
2021-02-04 10:50:11 -0500

    y2016d06abt

---
+++ advent/aoc2016/day06.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+import itertools  # noqa
+import re  # noqa
+from collections import Counter, defaultdict, deque  # noqa
+
+
+def parta(txt: str):
+    return error_correct_message(txt)
+
+
+def partb(txt: str):
+    return error_correct_message(txt, modified=True)
+
+
+def error_correct_message(txt, modified=False):
+    message = []
+    for chars in zip(*txt.splitlines()):
+        counts = Counter(chars)
+        most_common, *_, least_common = counts.most_common()
+        message.append((least_common if modified else most_common)[0])
+    return "".join(message)
+
+
+def main(txt: str):
+    print(f"parta: {parta(txt)}")
+    print(f"partb: {partb(txt)}")
+
+
+if __name__ == "__main__":
+    from aocd import data
+    main(data)
