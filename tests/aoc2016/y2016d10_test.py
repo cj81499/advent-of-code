9ae46d4db475a572bf828f9f7bee330c1c307147
Cal Jacobson <cj81499@users.noreply.github.com>
2021-02-09 14:33:39 -0500

    y2016d10abt

---
+++ tests/aoc2016/y2016d10_test.py
@@ -0,0 +1,14 @@
+import advent.aoc2016.day10 as d
+
+EXAMPLE_INPUT = """
+value 5 goes to bot 2
+bot 2 gives low to bot 1 and high to bot 0
+value 3 goes to bot 1
+bot 1 gives low to output 1 and high to bot 0
+bot 0 gives low to output 2 and high to output 0
+value 2 goes to bot 2
+""".strip()
+
+
+def test_a():
+    assert d.parta(EXAMPLE_INPUT, compare=(5, 2)) == 2
