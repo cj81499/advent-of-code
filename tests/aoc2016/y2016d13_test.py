2b8d6ca2ef5e5a87458b077b915c639849ec0495
Cal Jacobson <cj81499@users.noreply.github.com>
2021-02-16 00:02:55 -0500

    y2016d13abt

---
+++ tests/aoc2016/y2016d13_test.py
@@ -0,0 +1,23 @@
+import advent.aoc2016.day13 as d
+
+EXAMPLE_SPACE = """
+.#.####.##
+..#..#...#
+#....##...
+###.#.###.
+.##..#..#.
+..##....#.
+#...##.###
+""".strip()
+
+
+def test_is_wall():
+    lines = EXAMPLE_SPACE.splitlines()
+    fav_num = 10
+    for y in range(len(lines)):
+        for x in range(len(lines[0])):
+            assert lines[y][x] == "." if d.is_open(x, y, fav_num) else "#"
+
+
+def test_a():
+    assert d.parta("10", target_pos=(7, 4)) == 11
