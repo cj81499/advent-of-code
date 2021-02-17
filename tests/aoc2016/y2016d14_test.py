92dfa7576f80e6db3b2b25e7c56010efa823382b
Cal Jacobson <cj81499@users.noreply.github.com>
2021-02-17 17:07:13 -0500

    y2016d14abt

---
+++ tests/aoc2016/y2016d14_test.py
@@ -0,0 +1,16 @@
+import pytest
+
+import advent.aoc2016.day14 as d
+
+
+def test_a():
+    assert d.parta("abc") == 22728
+
+
+def test_key_stretch_hash():
+    assert d.key_stretch_hash("abc0") == "a107ff634856bb300138cac6568c0f24"
+
+
+@pytest.mark.slow
+def test_b():
+    assert d.partb("abc") == 22551
