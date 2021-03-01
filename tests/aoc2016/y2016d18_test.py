395f7bce37cd633e41fcc51f700468f36f52ea7a
Cal Jacobson <cj81499@users.noreply.github.com>
2021-03-01 01:03:31 -0500

    y2016d18abt

---
+++ tests/aoc2016/y2016d18_test.py
@@ -0,0 +1,41 @@
+import pytest
+
+import advent.aoc2016.day18 as d
+
+EXAMPLE_INPUT_1 = "..^^."
+EXAMPLE_INPUT_2 = ".^^.^.^^^^"
+
+EXAMPLE_MAP_1 = """
+..^^.
+.^^^^
+^^..^
+""".strip()
+
+
+EXAMPLE_MAP_2 = """
+.^^.^.^^^^
+^^^...^..^
+^.^^.^.^^.
+..^^...^^^
+.^^^^.^^.^
+^^..^.^^..
+^^^^..^^^.
+^..^^^^.^^
+.^^^..^.^^
+^^.^^^..^^
+""".strip()
+
+
+@pytest.mark.parametrize("first_row, expected", (
+    (EXAMPLE_INPUT_1, EXAMPLE_MAP_1),
+    (EXAMPLE_INPUT_2, EXAMPLE_MAP_2),
+))
+def test_next_row(first_row, expected):
+    prev = first_row
+    for row in expected.splitlines():
+        assert row == prev
+        prev = d.next_row(row)
+
+
+def test_a():
+    assert d.parta(EXAMPLE_INPUT_2, num_rows=10) == 38
