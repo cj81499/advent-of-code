66e82d84ec32b445ad47631d753423552720a907
Cal Jacobson <cj81499@users.noreply.github.com>
2021-11-28 21:08:06 -0600

    util changes

--- src/aoc_cj/util/heap.py
+++ src/aoc_cj/util/heap.py
@@ -1,37 +1,25 @@
 import heapq
-from typing import Generic, TypeVar
+from typing import Generic, Optional, TypeVar
 
 T = TypeVar("T")
 
 
 class Heap(Generic[T]):
-    def __init__(self, items: list[T] = []):
-        self.heap = items.copy()
-        heapq.heapify(self.heap)
+    def __init__(self, items: Optional[list[T]] = None):
+        self._heap = [] if items is None else items.copy()
+        heapq.heapify(self._heap)
 
     def __len__(self) -> int:
-        return len(self.heap)
+        return len(self._heap)
 
-    def push(self, item: T):
-        heapq.heappush(self.heap, item)
+    def push(self, item: T) -> None:
+        heapq.heappush(self._heap, item)
 
     def pop(self) -> T:
-        return heapq.heappop(self.heap)
+        return heapq.heappop(self._heap)
 
     def pushpop(self, item: T) -> T:
-        return heapq.heappushpop(self.heap, item)
+        return heapq.heappushpop(self._heap, item)
 
     def replace(self, item: T) -> T:
-        return heapq.heapreplace(self.heap, item)
-
-    @staticmethod
-    def merge(*iterables, key=None, reverse=False):
-        return heapq.merge(*iterables, key=key, reverse=reverse)
-
-    @staticmethod
-    def nlargest(n, iterable, key=None) -> list[T]:
-        return heapq.nlargest(n, iterable, key=key)
-
-    @staticmethod
-    def nsmallest(n, iterable, key=None) -> list[T]:
-        return heapq.nsmallest(n, iterable, key=key)
+        return heapq.heapreplace(self._heap, item)
--- src/aoc_cj/util/priority_queue.py
+++ src/aoc_cj/util/priority_queue.py
@@ -1,25 +1,26 @@
 import dataclasses
 from typing import Generic, TypeVar
 
-from aoc_cj.util import Heap
+from aoc_cj.util.heap import Heap
 
 T = TypeVar("T")
 
 
-class PriorityQueue(Generic[T]):
-    @dataclasses.dataclass(order=True)
-    class PrioritizedItem:
-        priority: int
-        item: T = dataclasses.field(compare=False)
+@dataclasses.dataclass(order=True)
+class PrioritizedItem(Generic[T]):
+    priority: int
+    item: T = dataclasses.field(compare=False)
 
-    def __init__(self):
-        self.q = Heap()
+
+class PriorityQueue(Generic[T]):
+    def __init__(self) -> None:
+        self._pq: Heap[PrioritizedItem[T]] = Heap()
 
     def __len__(self) -> int:
-        return len(self.q)
+        return len(self._pq)
 
-    def push(self, priority: int, item: T):
-        self.q.push(PriorityQueue.PrioritizedItem(priority, item))
+    def push(self, priority: int, item: T) -> None:
+        self._pq.push(PrioritizedItem(priority, item))
 
     def pop(self) -> T:
-        return self.q.pop().item
+        return self._pq.pop().item
