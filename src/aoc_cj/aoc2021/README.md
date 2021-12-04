54d3ac0a50ea0e762b196654a84147203ac829ae
Cal Jacobson <cj81499@users.noreply.github.com>
2021-12-04 00:14:14 -0600

    y2021d04abt

--- src/aoc_cj/aoc2021/README.md
+++ src/aoc_cj/aoc2021/README.md
@@ -5,7 +5,7 @@
 | 01  | [Sonar Sweep](https://adventofcode.com/2021/day/1)       | :star: | :star: | :heavy_check_mark: |
 | 02  | [Dive!](https://adventofcode.com/2021/day/2)             | :star: | :star: | :heavy_check_mark: |
 | 03  | [Binary Diagnostic](https://adventofcode.com/2021/day/3) | :star: | :star: | :heavy_check_mark: |
-| 04  | [?](https://adventofcode.com/2021/day/4)                 |  :x:   |  :x:   |        :x:         |
+| 04  | [Giant Squid](https://adventofcode.com/2021/day/4)       | :star: | :star: | :heavy_check_mark: |
 | 05  | [?](https://adventofcode.com/2021/day/5)                 |  :x:   |  :x:   |        :x:         |
 | 06  | [?](https://adventofcode.com/2021/day/6)                 |  :x:   |  :x:   |        :x:         |
 | 07  | [?](https://adventofcode.com/2021/day/7)                 |  :x:   |  :x:   |        :x:         |
@@ -33,6 +33,7 @@
 ```text
       -------Part 1--------   -------Part 2--------
 Day       Time  Rank  Score       Time  Rank  Score
+  4   00:10:17   203      0   00:15:59   302      0
   3   00:04:57   503      0   00:15:41   438      0
   2   00:01:42   122      0   00:05:06   931      0
   1   00:01:35   341      0   00:04:03   349      0
---
+++ src/aoc_cj/aoc2021/day04.py
@@ -0,0 +1,67 @@
+from itertools import chain
+from typing import Iterator
+
+
+class Board:
+    def __init__(self, grid: list[list[int]]):
+        self._grid = grid
+        self._marks = [[False for _ in row] for row in grid]
+        self._last_draw: int = -1
+
+    def mark_number(self, draw: int) -> None:
+        if self.is_won():
+            raise ValueError("cannot mark number after game is won")
+        self._last_draw = draw
+        for i, row in enumerate(self._grid):
+            for j, n in enumerate(row):
+                if n == draw:
+                    self._marks[i][j] = True
+
+    def is_won(self) -> bool:
+        return any(all(marks) for marks in self._marks) or any(all(marks) for marks in zip(*self._marks))
+
+    def score(self) -> int:
+        if not self.is_won():
+            raise ValueError("cannot get score until game is won")
+        unmarked_numbers = (n for n, marked in zip(chain(*self._grid), chain(*self._marks)) if not marked)
+        return sum(unmarked_numbers) * self._last_draw
+
+    @staticmethod
+    def parse(txt: str) -> "Board":
+        return Board([[int(n) for n in line.split()] for line in txt.splitlines()])
+
+
+def winners(draws: list[int], boards: list[Board]) -> Iterator[Board]:
+    for n in draws:
+        for b in (b for b in boards if not b.is_won()):
+            b.mark_number(n)
+            if b.is_won():
+                yield b
+
+
+def parse(txt: str) -> tuple[list[int], list[Board]]:
+    draws, *boards = txt.split("\n\n")
+    return [int(x) for x in draws.split(",")], [Board.parse(b) for b in boards]
+
+
+def parta(txt: str) -> int:
+    draws, boards = parse(txt)
+    first_winner = next(winners(draws, boards))
+    return first_winner.score()
+
+
+def partb(txt: str) -> int:
+    draws, boards = parse(txt)
+    *_rest, last_winner = winners(draws, boards)
+    return last_winner.score()
+
+
+def main(txt: str) -> None:
+    print(f"parta: {parta(txt)}")
+    print(f"partb: {partb(txt)}")
+
+
+if __name__ == "__main__":
+    from aocd import data
+
+    main(data)
