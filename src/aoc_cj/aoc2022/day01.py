54e4aedcd1b0e7c4f77b77f3557380acb07f8102
Cal Jacobson <cj81499@users.noreply.github.com>
2022-10-07 05:07:53 +0000

    scaffold y2022

---
+++ src/aoc_cj/aoc2022/day01.py
@@ -0,0 +1,23 @@
+import itertools
+import re
+from collections import Counter, defaultdict, deque
+
+
+def parta(txt: str) -> None:
+    print(txt)
+    return None
+
+
+def partb(txt: str) -> None:
+    return None
+
+
+def main(txt: str) -> None:
+    print(f"parta: {parta(txt)}")
+    print(f"partb: {partb(txt)}")
+
+
+if __name__ == "__main__":
+    from aocd import data
+
+    main(data)
---
+++ tests/aoc2022/y2022d01_test.py
@@ -0,0 +1,13 @@
+import aoc_cj.aoc2022.day01 as d
+
+EXAMPLE_INPUT = """
+sample input
+""".strip()
+
+
+def test_a():
+    assert d.parta(EXAMPLE_INPUT) is None
+
+
+def test_b():
+    assert d.partb(EXAMPLE_INPUT) is None
