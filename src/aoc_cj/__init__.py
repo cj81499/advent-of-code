c3939220c89d1b4bbcc188fb7aa36980028d2c1b
Cal Jacobson <cj81499@users.noreply.github.com>
2022-10-18 21:03:02 -0500

    chore: 2022 prep

--- src/aoc_cj/__init__.py
+++ src/aoc_cj/__init__.py
@@ -1,45 +1,49 @@
 """cj's solutions for https://adventofcode.com/"""
 
 import importlib
+import inspect
 import logging
 from pathlib import Path
-from typing import Union
+from typing import Literal, Optional, Union
 
-ADVENT_DIR = Path(__file__).parent
-SRC_DIR = ADVENT_DIR.parent
-PROJECT_ROOT = SRC_DIR.parent
+PROJECT_ROOT = Path(__file__).parent.parent.parent
+LOGS_DIR = PROJECT_ROOT / "logs"
+LOGS_DIR.mkdir(exist_ok=True)
 
-
-def _NULL(_):
-    return None
+Answer = Optional[Union[int, str]]
 
 
 logging.basicConfig(
-    filename=PROJECT_ROOT / "aoc_cj.log",
+    filename=LOGS_DIR / "aoc_cj.log",
     level=logging.DEBUG,
     format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
 )
 
-Answer = Union[int, str]
 
-
-def solve(year, day, data) -> tuple[Answer, Answer]:
-    day_str = f"{day:02d}"
-    logger = logging.getLogger(f"y{year} d{day_str}")
-
+def solve(year: int, day: int, data: str) -> tuple[Answer, Answer]:
     ans_a: Answer = None
     ans_b: Answer = None
 
     try:
-        module = importlib.import_module(f"aoc_cj.aoc{year}.day{day_str}")
-        ans_a = getattr(module, "parta", _NULL)(data)
-        ans_b = getattr(module, "partb", _NULL)(data)
+        module = importlib.import_module(f"{__name__}.aoc{year}.day{day:02d}")
+
+        def solve_part(part: Literal["a", "b"]) -> Answer:
+            if f := getattr(module, f"part{part}", None):
+                assert inspect.isfunction(f)
+                # TODO: consider checking inspect.signature
+                resp = f(data)
+                assert resp is None or isinstance(resp, (int, str))
+                return resp
+            return None
+
+        ans_a = solve_part("a")
+        ans_b = solve_part("b")
     except ModuleNotFoundError as e:
         raise NotImplementedError() from e
     except Exception as e:
-        logger.exception(e)
+        logging.exception("error while solving year=%s day=%s", year, day)
         raise e
     finally:
-        logger.info("got (%s, %s)", ans_a, ans_b)
+        logging.info("result for year=%s day=%s: (parta: %s, partb: %s)", year, day, ans_a, ans_b)
 
     return ans_a, ans_b
