c018ddc94ac2b4263f2b2d94dc38fab2cd66583e
Cal Jacobson <cj81499@users.noreply.github.com>
2022-11-30 23:15:03 -0600

    y2022d01

--- src/aoc_cj/aoc2022/day01.py
+++ src/aoc_cj/aoc2022/day01.py
@@ -1,10 +1,22 @@
 def parta(txt: str) -> None:
-    print(txt)
-    return None
+    chunks = txt.split("\n\n")
+
+    chunk_sums = [sum(int(n) for n in chunk.splitlines()) for chunk in chunks]
+    print(chunk_sums)
+    return max(chunk_sums)
 
 
 def partb(txt: str) -> None:
-    return None
+    chunks = txt.split("\n\n")
+
+    chunk_sums = [sum(int(n) for n in chunk.splitlines()) for chunk in chunks]
+    print(chunk_sums)
+    a = max(chunk_sums)
+    chunk_sums.remove(a)
+    b = max(chunk_sums)
+    chunk_sums.remove(b)
+    c = max(chunk_sums)
+    return a + b + c
 
 
 def main(txt: str) -> None:
--- tests/aoc2022/y2022d01_test.py
+++ tests/aoc2022/y2022d01_test.py
@@ -1,13 +1,26 @@
 import aoc_cj.aoc2022.day01 as d
 
 EXAMPLE_INPUT = """
-sample input
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
 """.strip()
 
 
 def test_a():
-    assert d.parta(EXAMPLE_INPUT) is None
+    assert d.parta(EXAMPLE_INPUT) == 24000
 
 
 def test_b():
-    assert d.partb(EXAMPLE_INPUT) is None
+    assert d.partb(EXAMPLE_INPUT) == 45000
