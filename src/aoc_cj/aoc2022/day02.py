5f6de4e0f38bab45a32db7b4ac2a751d68c2e55d
Cal Jacobson <cj81499@users.noreply.github.com>
2022-12-02 00:21:06 -0600

    y2022d02 cleanup

--- src/aoc_cj/aoc2022/day02.py
+++ src/aoc_cj/aoc2022/day02.py
@@ -1,68 +1,53 @@
-def move_score(op_move: str, my_move: str) -> int:
-    points = {"X": 1, "Y": 2, "Z": 3}[my_move]
+import enum
 
-    if op_move == "A":  # r
-        if my_move == "X":  # r
-            points += 3
-        elif my_move == "Y":  # p
-            points += 6
-        elif my_move == "Z":  # s
-            points += 0
-    elif op_move == "B":  # p
-        if my_move == "X":  # r
-            points += 0
-        elif my_move == "Y":  # p
-            points += 3
-        elif my_move == "Z":  # s
-            points += 6
-    elif op_move == "C":  # s
-        if my_move == "X":  # r
-            points += 6
-        elif my_move == "Y":  # p
-            points += 0
-        elif my_move == "Z":  # s
-            points += 3
 
-    return points
+class Move(enum.Enum):
+    ROCK = "A"
+    PAPER = "B"
+    SCISSORS = "C"
+
+    @staticmethod
+    def parse(s: str) -> "Move":
+        return {"X": Move.ROCK, "Y": Move.PAPER, "Z": Move.SCISSORS}[s]
+
+    def score(self) -> int:
+        return {Move.ROCK: 1, Move.PAPER: 2, Move.SCISSORS: 3}[self]
+
+    def play(self, against: "Move") -> "Outcome":
+        if self is against:
+            return Outcome.DRAW
+        return {
+            Move.ROCK: {Move.PAPER: Outcome.LOSE, Move.SCISSORS: Outcome.WIN},
+            Move.PAPER: {Move.ROCK: Outcome.WIN, Move.SCISSORS: Outcome.LOSE},
+            Move.SCISSORS: {Move.ROCK: Outcome.LOSE, Move.PAPER: Outcome.WIN},
+        }[self][against]
+
+
+class Outcome(enum.Enum):
+    LOSE = "X"
+    DRAW = "Y"
+    WIN = "Z"
+
+    def score(self) -> int:
+        return {Outcome.LOSE: 0, Outcome.DRAW: 3, Outcome.WIN: 6}[self]
+
+    def my_move(self, against: "Move") -> "Move":
+        if self is Outcome.DRAW:
+            return against
+        if self is Outcome.WIN:
+            return {Move.ROCK: Move.PAPER, Move.PAPER: Move.SCISSORS, Move.SCISSORS: Move.ROCK}[against]
+        # Outcome.LOSE
+        return {Move.ROCK: Move.SCISSORS, Move.PAPER: Move.ROCK, Move.SCISSORS: Move.PAPER}[against]
 
 
 def parta(txt: str) -> int:
-    total_score = 0
-    for turn in txt.splitlines():
-        op_move, my_move = turn.split()
-        total_score += move_score(op_move, my_move)
-
-    return total_score
+    turns = ((Move(op_move), Move.parse(my_move)) for op_move, my_move in (line.split() for line in txt.splitlines()))
+    return sum(my_move.score() + my_move.play(op_move).score() for op_move, my_move in turns)
 
 
 def partb(txt: str) -> int:
-    total_score = 0
-    for turn in txt.splitlines():
-        op_move, desired_outcome = turn.split()
-        my_move = {
-            # loss
-            "X": {
-                "A": "Z",
-                "B": "X",
-                "C": "Y",
-            },
-            # draw
-            "Y": {
-                "A": "X",
-                "B": "Y",
-                "C": "Z",
-            },
-            # win
-            "Z": {
-                "A": "Y",
-                "B": "Z",
-                "C": "X",
-            },
-        }[desired_outcome][op_move]
-
-        total_score += move_score(op_move, my_move)
-
-    return total_score
+    turns = ((Move(op_move), Outcome(outcome)) for op_move, outcome in (line.split() for line in txt.splitlines()))
+    return sum(outcome.my_move(op_move).score() + outcome.score() for op_move, outcome in turns)
 
 
 def main(txt: str) -> None:
