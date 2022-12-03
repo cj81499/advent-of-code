39f9f15d7e66cc17efd0f6c833fa5d7469b6bd46
Cal Jacobson <cj81499@users.noreply.github.com>
2022-12-02 23:42:27 -0600

    y2022d03 cleanup

--- src/aoc_cj/aoc2022/day03.py
+++ src/aoc_cj/aoc2022/day03.py
@@ -1,15 +1,13 @@
+from functools import reduce
+
 from more_itertools import chunked, only
 
 
-def parta(txt: str) -> int:
-    total = 0
-    for line in txt.splitlines():
-        first_half = line[: len(line) // 2]
-        second_half = line[len(line) // 2 :]
-        common_item = only(set(first_half).intersection(set(second_half)))
-        assert common_item is not None
-        total += priority(common_item)
-    return total
+def get_common_item(*strings: str) -> str:
+    intersection = reduce(lambda x, y: x & y, (set(s) for s in strings))
+    common = only(intersection)
+    assert common is not None
+    return common
 
 
 def priority(c: str) -> int:
@@ -17,16 +15,12 @@ def priority(c: str) -> int:
     return ord(c.lower()) - ord("a") + (1 if c.islower() else 27)
 
 
-def partb(txt: str) -> int:
-    total = 0
-    for chunk in chunked(txt.splitlines(), 3):
-        assert len(chunk) == 3
-        a, b, c = chunk
-        common_item = only(set(a).intersection(set(b)).intersection(set(c)))
-        assert common_item is not None
-        total += priority(common_item)
+def parta(txt: str) -> int:
+    return sum(priority(get_common_item(l[: len(l) // 2], l[len(l) // 2 :])) for l in txt.splitlines())
 
-    return total
+
+def partb(txt: str) -> int:
+    return sum(priority(get_common_item(*chunk)) for chunk in chunked(txt.splitlines(), 3))
 
 
 def main(txt: str) -> None:
