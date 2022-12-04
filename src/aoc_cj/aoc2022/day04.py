613eb822bbd9cc7c414c0cc27b5e68e3b9ef80b7
Cal Jacobson <cj81499@users.noreply.github.com>
2022-12-03 23:44:45 -0600

    y2022d04 cleanup

--- src/aoc_cj/aoc2022/day04.py
+++ src/aoc_cj/aoc2022/day04.py
@@ -1,30 +1,51 @@
-def parta(txt: str) -> int:
-    total = 0
-    for line in txt.splitlines():
-        a, b = line.split(",")
-        left = list(map(int, a.split("-")))
-        right = list(map(int, b.split("-")))
+from dataclasses import dataclass
+from typing import Union
 
-        if (left[0] <= right[0] and left[1] >= right[1]) or (left[0] >= right[0] and left[1] <= right[1]):
-            total += 1
-    return total
+from more_itertools import ilen
+
+
+@dataclass
+class SectionAssignment:
+    start: int
+    end: int
+
+    @staticmethod
+    def parse(s: str) -> "SectionAssignment":
+        return SectionAssignment(*map(int, s.split("-")))
+
+    def __contains__(self, item: Union[int, "SectionAssignment"]) -> bool:
+        if isinstance(item, int):
+            return self.start <= item <= self.end
+        return item.start in self and item.end in self
+
+
+@dataclass
+class SectionAssignmentPair:
+    left: SectionAssignment
+    right: SectionAssignment
+
+    @staticmethod
+    def parse(s: str) -> "SectionAssignmentPair":
+        return SectionAssignmentPair(*map(SectionAssignment.parse, s.split(",")))
+
+    def full_overlap(self) -> bool:
+        return self.left in self.right or self.right in self.left
+
+    def partial_overlap(self) -> bool:
+        return (
+            self.left.start in self.right
+            or self.left.end in self.right
+            or self.right.start in self.left
+            or self.right.end in self.left
+        )
+
+
+def parta(txt: str) -> int:
+    return ilen(1 for line in txt.splitlines() if SectionAssignmentPair.parse(line).full_overlap())
 
 
 def partb(txt: str) -> int:
-    total = 0
-    for line in txt.splitlines():
-        a, b = line.split(",")
-        left = list(map(int, a.split("-")))
-        right = list(map(int, b.split("-")))
-
-        if (
-            right[0] <= left[0] <= right[1]
-            or right[0] <= left[1] <= right[1]
-            or left[0] <= right[0] <= left[1]
-            or left[0] <= right[1] <= left[1]
-        ):
-            total += 1
-    return total
+    return ilen(1 for line in txt.splitlines() if SectionAssignmentPair.parse(line).partial_overlap())
 
 
 if __name__ == "__main__":
