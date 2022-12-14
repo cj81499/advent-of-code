645f33ffea5032d49c77210d25df163b132f1f06
Cal Jacobson <cj81499@users.noreply.github.com>
2022-12-13 23:34:02 -0600

    cleanup y2022d12

--- src/aoc_cj/aoc2022/day12.py
+++ src/aoc_cj/aoc2022/day12.py
@@ -6,7 +6,7 @@ import more_itertools as mi
 
 
 def parta(txt: str, part: Literal[1, 2] = 1) -> int:
-    grid = {x + (y * 1j): c for y, line in enumerate(txt.splitlines()) for x, c in enumerate(line)}
+    grid = {complex(x, y): c for y, line in enumerate(txt.splitlines()) for x, c in enumerate(line)}
 
     def adj(p: complex) -> Generator[complex, None, None]:
         yield from (adj_p for dir in (1j, -1j, 1, -1) if (adj_p := p + dir) in grid)
