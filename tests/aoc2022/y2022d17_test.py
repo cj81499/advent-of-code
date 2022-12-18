346db68f97a96b608246990026d007c937219e29
Cal Jacobson <cj81499@users.noreply.github.com>
2022-12-18 00:45:21 -0600

    skip tests rather than xfailing
    tests for y2021d20 fail, but the solution works on the actual input
    
    y2022d17b is unsolved, so there's no implementation to test

--- tests/aoc2022/y2022d17_test.py
+++ tests/aoc2022/y2022d17_test.py
@@ -9,6 +9,6 @@ def test_a() -> None:
     assert d.parta(EXAMPLE_INPUT) == 3068
 
 
-@pytest.mark.xfail
+@pytest.mark.skip("unsolved")
 def test_b() -> None:
-    assert d.partb(EXAMPLE_INPUT) == 1514285714288  # type: ignore[func-returns-value]
+    assert d.partb(EXAMPLE_INPUT) == 1514285714288
