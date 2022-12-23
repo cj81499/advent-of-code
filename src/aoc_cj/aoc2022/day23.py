34677b4e2acceaced0d8ff74ec95dfc607b944d3
Cal Jacobson <cj81499@users.noreply.github.com>
2022-12-23 00:35:48 -0600

    cleanup y2022d23

--- src/aoc_cj/aoc2022/day23.py
+++ src/aoc_cj/aoc2022/day23.py
@@ -1,12 +1,9 @@
-import itertools
 from collections import defaultdict, deque
 from collections.abc import Iterable
-from typing import DefaultDict
+from typing import DefaultDict, Deque, Literal
 
-import more_itertools as mi
-
-ELF = "#"
-EMPTY_GROUND = "."
+CardinalDirection = Literal["N", "W", "E", "S"]
+Direction = Literal[CardinalDirection, "NW", "NE", "SW", "SE"]
 
 NORTH = complex(0, -1)
 SOUTH = complex(0, 1)
@@ -14,144 +11,102 @@ WEST = complex(-1, 0)
 EAST = complex(1, 0)
 
 
-def elf_count(elf_positions: set[complex], to_check: Iterable[complex]) -> int:
-    return sum(1 for p in to_check if p in elf_positions)
+def adj(p: complex) -> dict[Direction, complex]:
+    return {
+        "NW": p + NORTH + WEST,
+        "N": p + NORTH,
+        "NE": p + NORTH + EAST,
+        "W": p + WEST,
+        "E": p + EAST,
+        "SW": p + SOUTH + WEST,
+        "S": p + SOUTH,
+        "SE": p + SOUTH + EAST,
+    }
 
 
-def parta(txt: str, *, rounds: int = 10) -> int:
-    elf_positions = {complex(x, y) for y, line in enumerate(txt.splitlines()) for x, c in enumerate(line) if c == ELF}
+class Simulation:
+    def __init__(self, elf_positions: set[complex]) -> None:
+        self._elves = elf_positions
+        self.is_complete = False
+        self.round = 0
+        self._check_order: Deque[CardinalDirection] = deque(("N", "S", "W", "E"))
 
-    check_order = deque("NSWE")
-    for round in range(rounds):
-        print(f"ROUND {round+1}")
-        print(f"check order:", [*check_order])
+    def simulate_round(self) -> None:
+        if self.is_complete:
+            raise AssertionError("simulation is already complete")
+        self.round += 1
 
-        # first half
+        proposed_moves = self._get_proposed_moves()  # first half of round
+        self._move_elves(proposed_moves)  # second half of round
+
+    def _get_proposed_moves(self) -> defaultdict[complex, set[complex]]:
+        # map position to position of elves that want to move there
         proposed_moves: DefaultDict[complex, set[complex]] = defaultdict(set)
-        # If no other Elves are in one of those eight positions, the Elf does not do anything during this round
-        for p in elf_positions:
-            nw = p + NORTH + WEST
-            n = p + NORTH
-            ne = p + NORTH + EAST
-            w = p + WEST
-            e = p + EAST
-            sw = p + SOUTH + WEST
-            s = p + SOUTH
-            se = p + SOUTH + EAST
 
-            # If no other Elves are in one of those eight positions, the Elf does not do anything during this round.
-            if elf_count(elf_positions, (nw, n, ne, w, e, sw, s, se)) == 0:
+        for elf_pos in self._elves:
+            adjacent = adj(elf_pos)
+
+            # if there are no adjacent elves, the elf does nothing
+            if not self.any_elf(adjacent.values()):
                 continue
 
-            CHECKS = {
-                "N": (n, ne, nw),
-                "S": (s, se, sw),
-                "W": (w, nw, sw),
-                "E": (e, ne, se),
-            }
+            for check_direction in self._check_order:
+                # if none of the spaces to the `check_direction` of are elves, an elf proposes a move in that direction
+                if not self.any_elf(adj_p for direction, adj_p in adjacent.items() if check_direction in direction):
+                    proposed_moves[adjacent[check_direction]].add(elf_pos)
+                    break  # an elf can only propose 1 move per turn
 
-            for c in check_order:
-                check = CHECKS[c]
-                if elf_count(elf_positions, check) == 0:
-                    proposed_moves[check[0]].add(p)
-                    break
-        # second half
-        # print(proposed_moves)
-        accepted_moves = {mi.only(srcs): dest for dest, srcs in proposed_moves.items() if len(srcs) == 1}
-        # print(accepted_moves)
-        # print()
+        return proposed_moves
 
-        # print("BEFORE")
-        # print(elf_positions)
-        for src, dest in accepted_moves.items():
-            assert src is not None
-            elf_positions.remove(src)
-            elf_positions.add(dest)
-        # print()
-        # print("AFTER")
-        # print(elf_positions)
-        print(f"END OF ROUND {round+1}")
-        for y in range(-2, 10):
-            for x in range(-3, 11):
-                print(ELF if complex(x, y) in elf_positions else EMPTY_GROUND, end="")
-            print()
-        # print(elf_positions)
-        print()
-        check_order.rotate(-1)
+    def _move_elves(self, proposed_moves: dict[complex, set[complex]]) -> None:
+        accepted_moves = {srcs.pop(): dest for dest, srcs in proposed_moves.items() if len(srcs) == 1}
 
-    min_x = min(int(p.real) for p in elf_positions)
-    max_x = max(int(p.real) for p in elf_positions)
-    min_y = min(int(p.imag) for p in elf_positions)
-    max_y = max(int(p.imag) for p in elf_positions)
+        if len(accepted_moves) == 0:
+            self.is_complete = True
+            return
 
-    dx = max_x - min_x + 1
-    dy = max_y - min_y + 1
-    area = dx * dy
+        # move the elves
+        self._elves.difference_update(accepted_moves)  # remove the old positions
+        self._elves.update(accepted_moves.values())  # add the new positions
 
-    print(f"{area=}, {dx=}, {dy=}, {min_x=}, {max_x=}, {min_y=}, {max_y=}")
-    print(elf_positions)
-    return area - len(elf_positions)
+        self._check_order.rotate(-1)
+
+    def covered_ground(self) -> int:
+        min_x = min(int(p.real) for p in self._elves)
+        max_x = max(int(p.real) for p in self._elves)
+        min_y = min(int(p.imag) for p in self._elves)
+        max_y = max(int(p.imag) for p in self._elves)
+
+        dx = max_x - min_x + 1
+        dy = max_y - min_y + 1
+
+        area = dx * dy
+
+        return area - len(self._elves)
+
+    @staticmethod
+    def parse(initial_state: str) -> "Simulation":
+        elf_positions = {
+            complex(x, y) for y, line in enumerate(initial_state.splitlines()) for x, c in enumerate(line) if c == "#"
+        }
+        return Simulation(elf_positions)
+
+    def any_elf(self, to_check: Iterable[complex]) -> bool:
+        return any(p in self._elves for p in to_check)
+
+
+def parta(txt: str) -> int:
+    s = Simulation.parse(txt)
+    for _round in range(10):
+        s.simulate_round()
+    return s.covered_ground()
 
 
 def partb(txt: str) -> int:
-    elf_positions = {complex(x, y) for y, line in enumerate(txt.splitlines()) for x, c in enumerate(line) if c == ELF}
-
-    check_order = deque("NSWE")
-    for round in itertools.count(start=1):
-        # first half
-        proposed_moves: DefaultDict[complex, set[complex]] = defaultdict(set)
-        # If no other Elves are in one of those eight positions, the Elf does not do anything during this round
-        for p in elf_positions:
-            nw = p + NORTH + WEST
-            n = p + NORTH
-            ne = p + NORTH + EAST
-            w = p + WEST
-            e = p + EAST
-            sw = p + SOUTH + WEST
-            s = p + SOUTH
-            se = p + SOUTH + EAST
-
-            # If no other Elves are in one of those eight positions, the Elf does not do anything during this round.
-            if elf_count(elf_positions, (nw, n, ne, w, e, sw, s, se)) == 0:
-                continue
-
-            CHECKS = {
-                "N": (n, ne, nw),
-                "S": (s, se, sw),
-                "W": (w, nw, sw),
-                "E": (e, ne, se),
-            }
-
-            for c in check_order:
-                check = CHECKS[c]
-                if elf_count(elf_positions, check) == 0:
-                    proposed_moves[check[0]].add(p)
-                    break
-        # second half
-        accepted_moves = {mi.only(srcs): dest for dest, srcs in proposed_moves.items() if len(srcs) == 1}
-
-        if len(accepted_moves) == 0:
-            return round
-
-        for src, dest in accepted_moves.items():
-            assert src is not None
-            elf_positions.remove(src)
-            elf_positions.add(dest)
-
-        check_order.rotate(-1)
-
-    min_x = min(int(p.real) for p in elf_positions)
-    max_x = max(int(p.real) for p in elf_positions)
-    min_y = min(int(p.imag) for p in elf_positions)
-    max_y = max(int(p.imag) for p in elf_positions)
-
-    dx = max_x - min_x + 1
-    dy = max_y - min_y + 1
-    area = dx * dy
-
-    print(f"{area=}, {dx=}, {dy=}, {min_x=}, {max_x=}, {min_y=}, {max_y=}")
-    print(elf_positions)
-    return area - len(elf_positions)
+    s = Simulation.parse(txt)
+    while not s.is_complete:
+        s.simulate_round()
+    return s.round
 
 
 if __name__ == "__main__":
