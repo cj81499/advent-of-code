16a5df10238f358150b4482e7159d635b8190ed0
Cal Jacobson <cj81499@users.noreply.github.com>
2022-12-23 18:41:40 -0600

    improve `aoc` plugin

--- src/aoc_cj/__init__.py
+++ src/aoc_cj/__init__.py
@@ -4,6 +4,7 @@ import importlib
 import inspect
 import logging
 from pathlib import Path
+from types import ModuleType
 from typing import Literal, Optional, Union
 
 PROJECT_ROOT = Path(__file__).parent.parent.parent
@@ -20,33 +21,35 @@ logging.basicConfig(
 )
 
 
-def solve(year: int, day: int, data: str) -> tuple[Answer, Answer]:
+def solve(year: int, day: int, data: str) -> tuple[Answer, Answer]:  # pragma: no cover
     ans_a: Answer = None
     ans_b: Answer = None
 
+    module_name = f"{__name__}.aoc{year}.day{day:02d}"
     try:
-        module = importlib.import_module(f"{__name__}.aoc{year}.day{day:02d}")
+        module = importlib.import_module(module_name)
 
-        def solve_part(part: Literal["a", "b"]) -> Answer:
-            if f := getattr(module, f"part{part}", None):
-                assert inspect.isfunction(f)
-                try:
-                    # TODO: consider checking that inspect.signature matches expected signature
-                    resp = f(data)
-                except NotImplementedError:  # unsolved
-                    resp = None
-                assert resp is None or isinstance(resp, (int, str))
-                return resp
-            return None
-
-        ans_a = solve_part("a")
-        ans_b = solve_part("b")
+        ans_a = _solve_part(module, data, "a")
+        ans_b = _solve_part(module, data, "b")
     except ModuleNotFoundError as e:
-        raise NotImplementedError() from e
+        raise NotImplementedError(f"module '{module_name}' not found") from e
     except Exception as e:
-        logging.exception("error while solving year=%s day=%s", year, day)
+        logging.exception("exception thrown while solving year=%s day=%s", year, day)
         raise e
     finally:
         logging.info("result for year=%s day=%s: (parta: %s, partb: %s)", year, day, ans_a, ans_b)
 
     return ans_a, ans_b
+
+
+def _solve_part(module: ModuleType, data: str, part: Literal["a", "b"]) -> Answer:  # pragma: no cover
+    if f := getattr(module, f"part{part}", None):
+        assert inspect.isfunction(f)
+        try:
+            # TODO: consider checking that inspect.signature matches expected signature
+            resp = f(data)
+            assert isinstance(resp, (int, str))
+            return resp
+        except NotImplementedError:  # unsolved
+            return None
+    return None
