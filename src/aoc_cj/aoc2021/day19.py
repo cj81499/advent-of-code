a0c7ab7741b3732c0b30e4860fb2373605bc9383
Cal Jacobson <cj81499@users.noreply.github.com>
2023-01-18 21:33:54 -0600

    wip y2021d19

--- src/aoc_cj/aoc2021/day19.py
+++ src/aoc_cj/aoc2021/day19.py
@@ -61,6 +61,62 @@ def parta(txt: str) -> int:
     assert False, "unsolved"
 
 
+# import enum
+# import itertools
+
+# Beacon = tuple[int, int, int]
+
+
+# class Order(enum.Enum):
+#     XYZ = enum.auto()
+#     XZY = enum.auto()
+#     YXZ = enum.auto()
+#     YZX = enum.auto()
+#     ZXY = enum.auto()
+#     ZYX = enum.auto()
+
+
+# class Sign(enum.Enum):
+#     POSITIVE = enum.auto()
+#     NEGATIVE = enum.auto()
+
+
+# class Scan:
+#     def __init__(self, n: int, beacons: list[Beacon]) -> None:
+#         self.n = n
+#         self.beacons = list(beacons)
+
+#     @staticmethod
+#     def parse(txt: str) -> "Scan":
+#         lines = iter(txt.splitlines())
+#         n = int(next(lines).strip("- scanner"))
+#         splits = (map(int, l.split(",")) for l in lines)
+#         beacons = ((a, b, c) for a, b, c in splits)
+#         return Scan(n, beacons)
+
+#     def __repr__(self) -> str:
+#         return f"Scan(n={self.n}, beacons={self.beacons})"
+
+
+# def parta(txt: str) -> None:
+#     scans = list(map(Scan.parse, txt.split("\n\n")))
+#     for s in scans:
+#         print(s)
+#         print()
+
+#     # flips/transforms
+#     # TODO: this isn't right. you can get orientations that aren't possible.
+#     for a, b, c in itertools.permutations("xyz", 3):
+#         for sign_a, sign_b, sign_c in itertools.product("+-", repeat=3):
+#             print(f"{sign_a}{a}, {sign_b}{b}, {sign_c}{c}")
+
+#     for order in Order:
+#         for sign1, sign2, sign3 in itertools.product(Sign, repeat=3):
+#             print(order, sign1, sign2, sign3)
+
+#     return None
+
+
 def partb(txt: str) -> None:
     return None
 
