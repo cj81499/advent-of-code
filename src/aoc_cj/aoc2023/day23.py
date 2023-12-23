b6d79afc51230690b020eb22ee0744ec4a87ce12
Cal Jacobson <cj81499@users.noreply.github.com>
2023-12-23 00:15:22 -0600

    refactor for 100% coverage

--- src/aoc_cj/aoc2023/day23.py
+++ src/aoc_cj/aoc2023/day23.py
@@ -9,23 +9,16 @@ DOWN = 1j
 LEFT = -1
 RIGHT = 1
 
+DIRECTION_DELTA_MAP = {"^": UP, "v": DOWN, "<": LEFT, ">": RIGHT}
+
+NextPositions = Callable[[complex, dict[complex, str]], Generator[complex, None, None]]
+
 
 def parta(txt: str) -> int:
-    grid = {complex(x, y): c for y, line in enumerate(txt.splitlines()) for x, c in enumerate(line)}
-    max_y = int(max(p.imag for p in grid))
-    start = mi.one(p for p, c in grid.items() if p.imag == 0 and c == ".")
-    goal = mi.one(p for p, c in grid.items() if p.imag == max_y and c == ".")
-
     def next_positions(pos: complex, grid: dict[complex, str]) -> Generator[complex, None, None]:
         current_grid_val = grid[pos]
-        if current_grid_val == "^":
-            yield pos + UP
-        elif current_grid_val == "v":
-            yield pos + DOWN
-        elif current_grid_val == "<":
-            yield pos + LEFT
-        elif current_grid_val == ">":
-            yield pos + RIGHT
+        if (delta := DIRECTION_DELTA_MAP.get(current_grid_val)) is not None:
+            yield pos + delta
         elif current_grid_val == ".":
             for delta in (UP, DOWN, LEFT, RIGHT):
                 new_pos = pos + delta
@@ -35,6 +28,17 @@ def parta(txt: str) -> int:
         else:
             assert False, f"unrecognized grid val: '{current_grid_val}'"
 
+    return solve(txt, next_positions)
+
+
+def solve(txt: str, next_positions: NextPositions) -> int:
+    grid = {complex(x, y): c for y, line in enumerate(txt.splitlines()) for x, c in enumerate(line)}
+
+    start = mi.one(p for p, c in grid.items() if p.imag == 0 and c == ".")
+
+    max_y = int(max(p.imag for p in grid))
+    goal = mi.one(p for p, c in grid.items() if p.imag == max_y and c == ".")
+
     return max(len(path) for path in paths_between(start, goal, grid, next_positions))
 
 
@@ -42,7 +46,7 @@ def paths_between(
     start: complex,
     goal: complex,
     grid: dict[complex, str],
-    next_positions: Callable[[complex, dict[complex, str]], Generator[complex, None, None]],
+    next_positions: NextPositions,
 ) -> Generator[frozenset[complex], None, None]:
     to_explore: deque[tuple[complex, frozenset[complex]]] = deque(((start, frozenset((start,))),))
     while to_explore:
@@ -56,21 +60,14 @@ def paths_between(
 
 
 def partb(txt: str) -> int:
-    # IDEA (not implemented yet. this is fast enough for the test case, but not fast enough for the real input)
+    # IDEA (not implemented yet. as-is, this is fast enough for the test case, but not fast enough for the real input)
+    #
     # https://www.reddit.com/r/adventofcode/comments/18oy4pc/comment/kekf0hl/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
     # there are some very long paths where there is no branching allowed.
     # By creating an adjacency graph and "merging" any nodes that have exactly 2
     # neighbors into those neighbors, we get a graph with only a couple dozen
     # nodes instead of many thousands. The same DFS as before now completes
     # reasonably quickly.
-
-    grid = {complex(x, y): c for y, line in enumerate(txt.splitlines()) for x, c in enumerate(line)}
-    max_y = int(max(p.imag for p in grid))
-    start = mi.one(p for p, c in grid.items() if p.imag == 0 and c == ".")
-    goal = mi.one(p for p, c in grid.items() if p.imag == max_y and c == ".")
-
-    theoretical_max = sum(1 for c in grid.values() if c == ".")
-    print(f"{theoretical_max=}")
 
     def next_positions(pos: complex, grid: dict[complex, str]) -> Generator[complex, None, None]:
         current_grid_val = grid[pos]
@@ -83,11 +80,7 @@ def partb(txt: str) -> int:
         else:
             assert False, f"unrecognized grid val: '{current_grid_val}'"
 
-    best = 0
-    for path in paths_between(start, goal, grid, next_positions):
-        print(len(path))
-        best = max(best, len(path))
-    return best
+    return solve(txt, next_positions)
 
 
 if __name__ == "__main__":
