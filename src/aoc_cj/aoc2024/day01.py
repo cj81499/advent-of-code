7435a1a71a741d40f3aec9e0e5bbcc698da796aa
Cal Jacobson <cj81499@users.noreply.github.com>
2024-11-29 22:28:19 -0600

    scaffold y2024d01

---
+++ src/aoc_cj/aoc2024/day01.py
@@ -0,0 +1,14 @@
+def part_1(txt: str) -> int:
+    print(txt)
+    raise NotImplementedError()
+
+
+def part_2(txt: str) -> int:
+    raise NotImplementedError()
+
+
+if __name__ == "__main__":
+    from aocd import data
+
+    print(f"part_1: {part_1(data)}")
+    print(f"part_2: {part_2(data)}")
---
+++ tests/aoc2024/y2024d01_test.py
@@ -0,0 +1,13 @@
+import aoc_cj.aoc2024.day01 as d
+
+EXAMPLE_INPUT = """
+sample input
+""".strip()
+
+
+def test_part_1() -> None:
+    assert d.part_1(EXAMPLE_INPUT) is None
+
+
+def test_part_2() -> None:
+    assert d.part_2(EXAMPLE_INPUT) is None
