eb602d94fe62ff2ec3d528487832a1e716e909c3
Cal Jacobson <cj81499@users.noreply.github.com>
2024-11-30 23:05:32 -0600

    y20224d01 - add tests

--- tests/aoc2024/y2024d01_test.py
+++ tests/aoc2024/y2024d01_test.py
@@ -1,13 +1,18 @@
 import aoc_cj.aoc2024.day01 as d
 
 EXAMPLE_INPUT = """
-sample input
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
 """.strip()
 
 
 def test_part_1() -> None:
-    assert d.part_1(EXAMPLE_INPUT) is None
+    assert d.part_1(EXAMPLE_INPUT) == 11
 
 
 def test_part_2() -> None:
-    assert d.part_2(EXAMPLE_INPUT) is None
+    assert d.part_2(EXAMPLE_INPUT) == 31
