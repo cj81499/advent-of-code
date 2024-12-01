535b989246a484ce8deb7a256681cbda81c4f0da
Cal Jacobson <cj81499@users.noreply.github.com>
2024-11-30 23:05:04 -0600

    y20224d01 - initial solve

--- src/aoc_cj/aoc2024/day01.py
+++ src/aoc_cj/aoc2024/day01.py
@@ -1,10 +1,34 @@
+import collections
+
+
 def part_1(txt: str) -> int:
-    print(txt)
-    raise NotImplementedError()
+    lefts = []
+    rights = []
+    for line in txt.splitlines():
+        l, r = line.split()
+        lefts.append(int(l))
+        rights.append(int(r))
+    lefts = sorted(lefts)
+    rights = sorted(rights)
+    s = 0
+    for l, r in zip(sorted(lefts), sorted(rights)):
+        diff = abs(l - r)
+        s += diff
+    return s
 
 
 def part_2(txt: str) -> int:
-    raise NotImplementedError()
+    sim_score = 0
+    lefts = []
+    rights = []
+    for line in txt.splitlines():
+        l, r = line.split()
+        lefts.append(int(l))
+        rights.append(int(r))
+    rights = collections.Counter(rights)
+    for l in lefts:
+        sim_score += l * rights[l]
+    return sim_score
 
 
 if __name__ == "__main__":
