5f146f9ea5095ce8f8725c9d578bf39a343729e6
Cal Jacobson <cj81499@users.noreply.github.com>
2024-11-30 23:23:46 -0600

    y20224d01 - polish

--- src/aoc_cj/aoc2024/day01.py
+++ src/aoc_cj/aoc2024/day01.py
@@ -1,34 +1,25 @@
 import collections
 
 
+def parse(txt: str) -> tuple[tuple[int, ...], tuple[int, ...]]:
+    ints_by_line = (tuple(int(x) for x in line.split()) for line in txt.splitlines())
+    transpose: zip[tuple[int, ...]] = zip(*ints_by_line)
+    tup = tuple(transpose)
+    if len(tup) != 2:
+        raise ValueError
+    return tup
+
+
 def part_1(txt: str) -> int:
-    lefts = []
-    rights = []
-    for line in txt.splitlines():
-        l, r = line.split()
-        lefts.append(int(l))
-        rights.append(int(r))
-    lefts = sorted(lefts)
-    rights = sorted(rights)
-    s = 0
-    for l, r in zip(sorted(lefts), sorted(rights)):
-        diff = abs(l - r)
-        s += diff
-    return s
+    left_col, right_col = parse(txt)
+    return sum(abs(l - r) for l, r in zip(sorted(sorted(left_col)), sorted(sorted(right_col))))
 
 
 def part_2(txt: str) -> int:
-    sim_score = 0
-    lefts = []
-    rights = []
-    for line in txt.splitlines():
-        l, r = line.split()
-        lefts.append(int(l))
-        rights.append(int(r))
-    rights = collections.Counter(rights)
-    for l in lefts:
-        sim_score += l * rights[l]
-    return sim_score
+    left_col, right_col = parse(txt)
+    right_counts = collections.Counter(right_col)
+
+    return sum(l * right_counts[l] for l in left_col)
 
 
 if __name__ == "__main__":
