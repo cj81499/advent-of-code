05e843657610e7aff1fbe92de6345f24b5acbd6b
Cal Jacobson <cj81499@users.noreply.github.com>
2024-12-01 18:24:21 -0600

    y2015d18 rework

--- src/aoc_cj/aoc2015/day18.py
+++ src/aoc_cj/aoc2015/day18.py
@@ -1,93 +1,79 @@
-import collections
-from typing import Generator, override
+import contextlib
+import enum
+from collections import Counter
+from collections.abc import Generator
+from typing import assert_never
 
-ON = "#"
-OFF = "."
-
-
-class Grid:
-    def __init__(self, txt: str, include_diagonals: bool = False) -> None:
-        lines = txt.splitlines()
-
-        self.height = len(lines)
-        self.width = len(lines[0])
-        assert all(len(line) == self.width for line in lines)
-
-        self._grid: dict[tuple[int, int], str] = {}
-        for y, row in enumerate(lines):
-            for x, c in enumerate(row):
-                self._grid[(x, y)] = c
-
-        self.include_diagonals = include_diagonals
-
-    def at(self, x: int, y: int) -> str | None:
-        return self._grid.get((x, y), None)
-
-    def put(self, x: int, y: int, val: str) -> str | None:
-        old = self.at(x, y)
-        self._grid[(x, y)] = val
-        return old
-
-    def adj(self, x: int, y: int) -> Generator[str | None, None, None]:
-        yield from (self.at(x, y) for (x, y) in self.adj_pos(x, y))
-
-    def adj_pos(self, x: int, y: int) -> Generator[tuple[int, int], None, None]:
-        yield from ((x + dx, y + dy) for (dx, dy) in ((0, -1), (0, 1), (-1, 0), (1, 0)))
-        if self.include_diagonals:
-            yield from ((x + dx, y + dy) for (dx, dy) in ((-1, -1), (1, -1), (1, 1), (-1, 1)))
-
-    @override
-    def __str__(self) -> str:
-        return "\n".join("".join(self.at(x, y) or " " for x in range(self.width)) for y in range(self.height))
+from more_itertools import nth
 
 
-class Lightshow:
-    def __init__(self, txt: str, corners_locked: bool = False) -> None:
-        self._grid = Grid(txt, include_diagonals=True)
-        self.corners_locked = corners_locked
-        self.turn_on_corners(self._grid)
-
-    def turn_on_corners(self, grid: Grid) -> None:
-        if self.corners_locked:
-            grid.put(0, 0, ON)
-            grid.put(grid.width - 1, 0, ON)
-            grid.put(grid.width - 1, grid.height - 1, ON)
-            grid.put(0, grid.height - 1, ON)
-
-    def simulate(self, steps: int) -> "Lightshow":
-        g = self._grid
-        for step in range(steps):
-            g = self._next_grid(g)
-        return Lightshow(str(g))
-
-    def _next_grid(self, grid: Grid) -> Grid:
-        rows = []
-        for y in range(grid.height):
-            row = []
-            for x in range(grid.width):
-                state = grid.at(x, y)
-                new_state = OFF
-                on_neighbors = collections.Counter(grid.adj(x, y))[ON]
-                if state == ON and on_neighbors in (2, 3):
-                    new_state = ON
-                elif state == OFF and on_neighbors == 3:
-                    new_state = ON
-                row.append(new_state)
-            rows.append("".join(row))
-        g = Grid("\n".join(rows), include_diagonals=True)
-        self.turn_on_corners(g)
-        return g
-
-    def count(self) -> collections.Counter[str]:
-        return collections.Counter(str(self._grid))
+class LightState(enum.StrEnum):
+    ON = "#"
+    OFF = "."
 
 
-def part_1(txt: str, steps: int = 100) -> int:
-    return Lightshow(txt).simulate(steps).count()[ON]
+LightshowState = list[list[LightState]]
 
 
-def part_2(txt: str, steps: int = 100) -> int:
-    return Lightshow(txt, corners_locked=True).simulate(steps).count()[ON]
+def parse_lightshow_state(txt: str) -> LightshowState:
+    return [[LightState(c) for c in line] for line in txt.splitlines()]
+
+
+def lightshow(initial_state: str, *, corners_stuck_on: bool = False) -> Generator[LightshowState]:
+    state = parse_lightshow_state(initial_state)
+    while True:
+        if corners_stuck_on:
+            state[0][0] = LightState.ON
+            state[0][-1] = LightState.ON
+            state[-1][0] = LightState.ON
+            state[-1][-1] = LightState.ON
+        yield state
+        new_state = []
+        for y, row in enumerate(state):
+            new_row = []
+            for x, initial_light_state in enumerate(row):
+                neighbor_counts = Counter(neighbor_values(state, x, y))
+                if initial_light_state == LightState.ON:
+                    light_state = LightState.ON if neighbor_counts[LightState.ON] in (2, 3) else LightState.OFF
+                elif initial_light_state == LightState.OFF:
+                    light_state = LightState.ON if neighbor_counts[LightState.ON] == 3 else LightState.OFF
+                else:
+                    assert_never(initial_light_state)
+                new_row.append(light_state)
+            new_state.append(new_row)
+        state = new_state
+
+
+def neighbor_values(grid: LightshowState, x: int, y: int) -> Generator[LightState]:
+    neighbors = (
+        (x - 1, y - 1),
+        (x, y - 1),
+        (x + 1, y - 1),
+        (x - 1, y),
+        (x + 1, y),
+        (x - 1, y + 1),
+        (x, y + 1),
+        (x + 1, y + 1),
+    )
+    for nx, ny in neighbors:
+        if nx >= 0 and ny >= 0:
+            with contextlib.suppress(IndexError):
+                yield grid[ny][nx]
+
+
+def part_1(txt: str, *, steps: int = 100) -> int:
+    return _solve(txt, steps)
+
+
+def part_2(txt: str, *, steps: int = 100) -> int:
+    return _solve(txt, steps, corners_stuck_on=True)
+
+
+def _solve(txt: str, steps: int, *, corners_stuck_on: bool = False) -> int:
+    lightshow_simulation = lightshow(txt, corners_stuck_on=corners_stuck_on)
+    end_state = nth(lightshow_simulation, steps)
+    assert end_state is not None
+    return sum(sum(1 for c in row if c == LightState.ON) for row in end_state)
 
 
 if __name__ == "__main__":
--- tests/aoc2015/y2015d18_test.py
+++ tests/aoc2015/y2015d18_test.py
@@ -1,6 +1,9 @@
+import pytest
+from more_itertools import nth
+
 import aoc_cj.aoc2015.day18 as d
 
-EXAMPLE_INPUT = """
+INITIAL_1 = """
 .#.#.#
 ...##.
 #....#
@@ -9,10 +12,124 @@ EXAMPLE_INPUT = """
 ####..
 """.strip()
 
+AFTER_ONE_1 = """
+..##..
+..##.#
+...##.
+......
+#.....
+#.##..
+""".strip()
 
-def test_part_1():
-    assert d.part_1(EXAMPLE_INPUT, steps=4) == 4
+AFTER_TWO_1 = """
+..###.
+......
+..###.
+......
+.#....
+.#....
+""".strip()
+
+AFTER_THREE_1 = """
+...#..
+......
+...#..
+..##..
+......
+......
+""".strip()
+
+AFTER_FOUR_1 = """
+......
+......
+..##..
+..##..
+......
+......
+""".strip()
 
 
-def test_part_2():
-    assert d.part_2(EXAMPLE_INPUT, steps=5) == 17
+INITIAL_2 = """
+##.#.#
+...##.
+#....#
+..#...
+#.#..#
+####.#
+""".strip()
+
+AFTER_ONE_2 = """
+#.##.#
+####.#
+...##.
+......
+#...#.
+#.####
+""".strip()
+
+AFTER_TWO_2 = """
+#..#.#
+#....#
+.#.##.
+...##.
+.#..##
+##.###
+""".strip()
+
+AFTER_THREE_2 = """
+#...##
+####.#
+..##.#
+......
+##....
+####.#
+""".strip()
+
+AFTER_FOUR_2 = """
+#.####
+#....#
+...#..
+.##...
+#.....
+#.#..#
+""".strip()
+
+AFTER_FIVE_2 = """
+##.###
+.##..#
+.##...
+.##...
+#.#...
+##...#
+""".strip()
+
+
+@pytest.mark.parametrize(
+    ("initial", "steps", "expected", "corners_stuck_on"),
+    (
+        # part 1
+        (INITIAL_1, 1, AFTER_ONE_1, False),
+        (INITIAL_1, 2, AFTER_TWO_1, False),
+        (INITIAL_1, 3, AFTER_THREE_1, False),
+        (INITIAL_1, 4, AFTER_FOUR_1, False),
+        # part 2
+        (INITIAL_2, 1, AFTER_ONE_2, True),
+        (INITIAL_2, 2, AFTER_TWO_2, True),
+        (INITIAL_2, 3, AFTER_THREE_2, True),
+        (INITIAL_2, 4, AFTER_FOUR_2, True),
+        (INITIAL_2, 5, AFTER_FIVE_2, True),
+    ),
+)
+def test_simulation(initial: str, steps: int, expected: str, corners_stuck_on: bool) -> None:
+    simulation = d.lightshow(initial, corners_stuck_on=corners_stuck_on)
+    result = nth(simulation, steps)
+    assert result is not None, "simulation goes forever"
+    assert ["".join(row) for row in result] == [line for line in expected.splitlines()]
+
+
+def test_part_1() -> None:
+    assert d.part_1(INITIAL_1, steps=4) == 4
+
+
+def test_part_2() -> None:
+    assert d.part_2(INITIAL_2, steps=5) == 17
