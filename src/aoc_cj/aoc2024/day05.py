729e135f3f8e296bbad0177f9b7dfdcd2f56d056
Cal Jacobson <cj81499@users.noreply.github.com>
2024-12-04 23:41:58 -0600

    y2024d05 cleanup

--- src/aoc_cj/aoc2024/day05.py
+++ src/aoc_cj/aoc2024/day05.py
@@ -5,25 +5,23 @@ from typing import TypeVar
 import more_itertools as mi
 
 
-def is_correctly_ordered(rules: Mapping[int, set[int]], nums: Sequence[int]) -> bool:
-    for i, n in enumerate(nums):
-        after = nums[i + 1 :]
-        must_come_before = rules[n]
-        for n2 in after:
-            if n2 in must_come_before:
-                return False
+def parse(txt: str) -> tuple[str, Mapping[int, set[int]]]:
+    page_ordering_rules_s, pages_to_produce = txt.split("\n\n")
+    rules = defaultdict[int, set[int]](set)
+    for line in page_ordering_rules_s.splitlines():
+        l, r = map(int, line.split("|"))
+        rules[r].add(l)
+    return pages_to_produce, rules
+
+
+def is_correctly_ordered(rules: Mapping[int, set[int]], page_order: Sequence[int]) -> bool:
+    for i, n in enumerate(page_order):
+        after = page_order[i + 1 :]
+        must_come_prior_to_n = rules[n]
+        # if any number that must come prior to n appears after n, the pages are not in the correct order
+        if any(n2 in must_come_prior_to_n for n2 in after):
+            return False
     return True
-
-
-def fix_ordering(rules: Mapping[int, set[int]], nums: Sequence[int]) -> tuple[int, ...]:
-    order = []
-    to_order = set(nums)
-    while to_order:
-        # the next number is not "blocked" by any number in its rules that hasn't already been added to the order
-        chosen = mi.one(n for n in to_order if all(n2 in order for n2 in rules[n].intersection(nums)))
-        order.append(chosen)
-        to_order.remove(chosen)
-    return tuple(order)
 
 
 _T = TypeVar("_T")
@@ -33,41 +31,44 @@ def middle_element(seq: Sequence[_T]) -> _T:
     return seq[len(seq) // 2]
 
 
+def fix_ordering(rules: Mapping[int, set[int]], page_order: Sequence[int]) -> tuple[int, ...]:
+    order: list[int] = []
+    to_order = set(page_order)
+    while to_order:
+        # the next number in the order is not "blocked" by any number in its rules that hasn't already been added to the order
+        chosen = mi.one(
+            n
+            for n in to_order
+            if len(
+                rules[n]
+                # remove rules that deal with numbers that aren't in the sequence we're working with
+                .intersection(page_order)
+                # remove rules that are satisfied because they're already positioned prior to n
+                .difference(order)
+            )
+            == 0
+        )
+        order.append(chosen)
+        to_order.remove(chosen)
+    return tuple(order)
+
+
 def part_1(txt: str) -> int:
-    page_ordering_rules_s, pages_to_produce = txt.split("\n\n")
-    rules = defaultdict[int, set[int]](set)
-    for line in page_ordering_rules_s.splitlines():
-        l, r = map(int, line.split("|"))
-        rules[r].add(l)
-
-    s = 0
-
-    for line in pages_to_produce.splitlines():
-        nums = tuple(map(int, line.split(",")))
-
-        if is_correctly_ordered(rules, nums):
-            s += middle_element(nums)
-
-    return s
+    pages_to_produce, rules = parse(txt)
+    return sum(
+        middle_element(page_order)
+        for line in pages_to_produce.splitlines()
+        if is_correctly_ordered(rules, page_order := tuple(map(int, line.split(","))))
+    )
 
 
 def part_2(txt: str) -> int:
-    page_ordering_rules_s, pages_to_produce = txt.split("\n\n")
-    rules = defaultdict[int, set[int]](set)
-    for line in page_ordering_rules_s.splitlines():
-        l, r = map(int, line.split("|"))
-        rules[r].add(l)
-
-    s = 0
-
-    for line in pages_to_produce.splitlines():
-        nums = tuple(map(int, line.split(",")))
-
-        if not is_correctly_ordered(rules, nums):
-            correctly_ordered_nums = fix_ordering(rules, nums)
-            s += middle_element(correctly_ordered_nums)
-
-    return s
+    pages_to_produce, rules = parse(txt)
+    return sum(
+        middle_element(fix_ordering(rules, page_order))
+        for line in pages_to_produce.splitlines()
+        if not is_correctly_ordered(rules, page_order := tuple(map(int, line.split(","))))
+    )
 
 
 if __name__ == "__main__":
