c1a170d599c31c82bad707ed3326b26882cc79e6
Cal Jacobson <cj81499@users.noreply.github.com>
2024-12-10 23:59:39 -0600

    optimize

--- src/aoc_cj/aoc2024/day11.py
+++ src/aoc_cj/aoc2024/day11.py
@@ -1,8 +1,8 @@
 import functools
-import itertools
 from collections import Counter
 
 
+# This cache isn't essential, but it does speeds things up a tad bit.
 @functools.cache
 def advance_stone(n: int) -> tuple[int] | tuple[int, int]:
     if n == 0:
@@ -13,11 +13,7 @@ def advance_stone(n: int) -> tuple[int] | tuple[int, int]:
 
 
 def part_1(txt: str) -> int:
-    # original solution
-    nums = tuple(map(int, txt.split()))
-    for _ in range(25):
-        nums = tuple(itertools.chain.from_iterable(map(advance_stone, nums)))
-    return len(nums)
+    return solve(txt, blinks=25)
 
 
 def part_2(txt: str) -> int:
@@ -25,12 +21,18 @@ def part_2(txt: str) -> int:
 
 
 def solve(txt: str, *, blinks: int) -> int:
+    # for low numbers of blinks (part 1), we can simply simulate
+    # for part two, the number of stones to simulate is too large.
+    # observe that the order of stones does not matter
+    # instead of keeping track of each stone in order, keep track of how many
+    # stones we have with each number on it. that way, we can simulate all
+    # stones with the same number on them at once.
     stones = Counter(map(int, txt.split()))
     for _ in range(blinks):
         new_stones = Counter[int]()
-        for s, count in stones.items():
-            for new_stone in advance_stone(s):
-                new_stones[new_stone] += count
+        for stone_num, count in stones.items():
+            for new_stone_num in advance_stone(stone_num):
+                new_stones[new_stone_num] += count
         stones = new_stones
     return stones.total()
 
