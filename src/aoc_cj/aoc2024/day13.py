67d96e3d40be7cc41c302e1a51988e6b57d236fd
Cal Jacobson <cj81499@users.noreply.github.com>
2024-12-12 23:43:55 -0600

    tidy

--- src/aoc_cj/aoc2024/day13.py
+++ src/aoc_cj/aoc2024/day13.py
@@ -1,11 +1,8 @@
 import dataclasses
-import itertools
 
-import z3
+import z3  # type: ignore[import-untyped]
 
-from aoc_cj.util import ints
-
-_CONVERSION_ERROR = 10_000_000_000_000
+from aoc_cj import util
 
 
 @dataclasses.dataclass
@@ -16,53 +13,48 @@ class Game:
 
     @staticmethod
     def parse(txt: str) -> "Game":
-        a, b, prize = txt.splitlines()
-        return Game(
-            a=tuple(ints(a)),
-            b=tuple(ints(b)),
-            prize=tuple(ints(prize)),
-        )
+        a, b, prize = map(tuple, map(util.ints, txt.splitlines()))
+        assert len(a) == 2
+        assert len(b) == 2
+        assert len(prize) == 2
+        return Game(a=a, b=b, prize=prize)
 
-    def cheapest_way_to_win(self) -> int | None:
-        min_cost: int | None = None
-        for a_presses, b_presses in itertools.product(range(100), range(100)):
-            end_pos = (self.a[0] * a_presses + self.b[0] * b_presses, self.a[1] * a_presses + self.b[1] * b_presses)
-            if end_pos == self.prize:
-                cost = a_presses * 3 + b_presses
-                min_cost = cost if min_cost is None else min(cost, min_cost)
-        return min_cost
-
-    def cheapest_way_to_win2(self) -> int | None:
-        a_presses = z3.Int("a_presses")
-        b_presses = z3.Int("b_presses")
-
-        end_x = self.a[0] * a_presses + self.b[0] * b_presses
-        end_y = self.a[1] * a_presses + self.b[1] * b_presses
-
-        cost = z3.Int("cost")
-
+    def cheapest_way_to_win(self, off_by: int = 0) -> int | None:
+        # create an optimization problem
         opt = z3.Optimize()
-        opt.add(end_x == self.prize[0] + _CONVERSION_ERROR)
-        opt.add(end_y == self.prize[1] + _CONVERSION_ERROR)
+
+        # describe the game to the optimizer
+        a_presses, b_presses, cost = z3.Ints(("a_presses", "b_presses", "cost"))
+
+        x = self.a[0] * a_presses + self.b[0] * b_presses
+        y = self.a[1] * a_presses + self.b[1] * b_presses
+
+        opt.add(x == self.prize[0] + off_by)
+        opt.add(y == self.prize[1] + off_by)
         opt.add(cost == a_presses * 3 + b_presses)
+
+        # minimize cost to reach the prize
         opt.minimize(cost)
 
-        # solve the problem
-        is_sat = opt.check()
-        if is_sat != z3.sat:
+        # if there's no way to win the prize
+        if opt.check() != z3.sat:
             return None
+
+        # return the minimal cost to win the prize
         model = opt.model()
         return int(model[cost].as_long())
 
 
 def part_1(txt: str) -> int:
-    games = list(map(Game.parse, txt.split("\n\n")))
-    return sum(g.cheapest_way_to_win() or 0 for g in games)
+    return solve(txt)
 
 
 def part_2(txt: str) -> int:
-    games = list(map(Game.parse, txt.split("\n\n")))
-    return sum(g.cheapest_way_to_win2() or 0 for g in games)
+    return solve(txt, off_by=10_000_000_000_000)
+
+
+def solve(txt: str, off_by: int = 0) -> int:
+    return sum(g.cheapest_way_to_win(off_by=off_by) or 0 for g in map(Game.parse, txt.split("\n\n")))
 
 
 if __name__ == "__main__":
