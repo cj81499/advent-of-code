2c1a466a75abb66ea7031db8a3688a2a990c37ce
Cal Jacobson <cj81499@users.noreply.github.com>
2024-12-23 00:58:44 -0600

    networkx simplify

--- src/aoc_cj/aoc2024/day23.py
+++ src/aoc_cj/aoc2024/day23.py
@@ -1,46 +1,28 @@
-from collections import defaultdict
-from collections.abc import Mapping
-
 import networkx as nx
-
-Connections = Mapping[str, set[str]]
 
 
 def part_1(txt: str) -> int:
-    connections = _parse_connections(txt)
-
-    groups = {
-        frozenset((c1, c2, c3))
-        for c1, c1_connected_to in connections.items()
-        for c2 in c1_connected_to
-        for c3 in c1_connected_to & connections[c2]
-    }
-
+    g = _parse_graph(txt)
+    groups = {frozenset((cpu1, cpu2, cpu3)) for cpu1 in g for cpu2 in g[cpu1] for cpu3 in g[cpu1].keys() & g[cpu2]}
     groups_with_chief_historian = {g for g in groups if any(c.startswith("t") for c in g)}
-
     return len(groups_with_chief_historian)
 
 
 def part_2(txt: str) -> str:
-    connections = _parse_connections(txt)
-
-    # g = nx.Graph(connections)  # this works at runtime, but the type annotations for networkx are wrong :(  # noqa: ERA001
-    g = nx.Graph((c1, c2) for c1, c1_conns in connections.items() for c2 in c1_conns)
-
+    g = _parse_graph(txt)
     # https://networkx.org/documentation/stable/reference/algorithms/clique.html
     biggest_clique = max(nx.find_cliques(g), key=len)
-
     return ",".join(sorted(biggest_clique))
 
 
-def _parse_connections(txt: str) -> Connections:
-    connections = defaultdict[str, set[str]](set)
-    for line in txt.splitlines():
-        l, sep, r = line.partition("-")
-        assert sep == "-"
-        connections[l].add(r)
-        connections[r].add(l)
-    return connections
+def _parse_graph(txt: str) -> "nx.Graph[str]":
+    return nx.Graph(map(_parse_line, txt.splitlines()))
+
+
+def _parse_line(line: str) -> tuple[str, str]:
+    l, sep, r = line.partition("-")
+    assert sep == "-"
+    return l, r
 
 
 if __name__ == "__main__":
