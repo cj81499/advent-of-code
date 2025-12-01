dbb9a0bfc07f9eb69eb95d352d8b266a2149e9be
Cal Jacobson <cj81499@users.noreply.github.com>
2025-12-01 07:11:33 +0100

    y2025d01 initial solution

--- src/aoc_cj/aoc2025/day01.py
+++ src/aoc_cj/aoc2025/day01.py
@@ -1,10 +1,36 @@
 def part_1(txt: str) -> int:
-    print(txt)
-    raise NotImplementedError()
+    dial_pos = 50
+    zero_count = 0
+    for rotation in txt.splitlines():
+        direction = rotation[0]
+        distance = int(rotation[1:])
+        if direction == "L":
+            dial_pos = (dial_pos - distance) % 100
+        elif direction == "R":
+            dial_pos = (dial_pos + distance) % 100
+        else:
+            assert False, f"unreachable. rotation must start with 'L' or 'R'. Got '{direction}'"
+        if dial_pos == 0:
+            zero_count += 1
+    return zero_count
 
 
 def part_2(txt: str) -> int:
-    raise NotImplementedError()
+    dial_pos = 50
+    zero_count = 0
+    for rotation in txt.splitlines():
+        direction = rotation[0]
+        pos_distance = int(rotation[1:])
+        assert direction in ("L", "R"), f"rotation must start with 'L' or 'R'. Got '{direction}'"
+        if direction == "L":
+            move = -1
+        else:
+            move = 1
+        for _ in range(pos_distance):
+            dial_pos = (dial_pos + move) % 100
+            if dial_pos == 0:
+                zero_count += 1
+    return zero_count
 
 
 if __name__ == "__main__":
