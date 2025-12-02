e4367b417dc1f7d30c4d905ab1797bafa656c22a
Cal Jacobson <cj81499@users.noreply.github.com>
2025-12-02 10:28:05 +0100

    y2025d02 cleanup

--- src/aoc_cj/aoc2025/day02.py
+++ src/aoc_cj/aoc2025/day02.py
@@ -1,37 +1,26 @@
-import more_itertools
+import itertools
+import re
+from collections.abc import Generator
 
 
-def part_1(txt: str) -> int:
-    ranges = txt.replace("\n", "").split(",")
-    tot = 0
-    for r in ranges:
+def ranges(txt: str) -> Generator[range]:
+    for r in txt.replace("\n", "").split(","):
         start, sep, end = r.partition("-")
         assert sep == "-"
-        start, end = int(start), int(end)
-        for n in range(start, end + 1):
-            s = str(n)
-            l = len(s)
-            if l % 2 == 0 and s[: l // 2] == s[l // 2 :]:
-                tot += n
-    return tot
+        yield range(int(start), int(end) + 1)
+
+
+def part_1(txt: str, *, pattern: re.Pattern[str] = re.compile(r"^(.+)\1$")) -> int:
+    return sum(
+        filter(
+            lambda n: pattern.match(str(n)) is not None,
+            itertools.chain.from_iterable(ranges(txt)),
+        )
+    )
 
 
 def part_2(txt: str) -> int:
-    ranges = txt.replace("\n", "").split(",")
-    tot = 0
-    for r in ranges:
-        start, sep, end = r.partition("-")
-        assert sep == "-"
-        start, end = int(start), int(end)
-        for n in range(start, end + 1):
-            s = str(n)
-            l = len(s)
-            if any(
-                l % chunk_size == 0 and more_itertools.all_equal(more_itertools.chunked(s, chunk_size, strict=True))
-                for chunk_size in range(1, (len(s) // 2) + 1)
-            ):
-                tot += n
-    return tot
+    return part_1(txt, pattern=re.compile(r"^(.+)\1+$"))
 
 
 if __name__ == "__main__":
