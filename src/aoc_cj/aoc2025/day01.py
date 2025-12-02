225d81144a4736e08654734deedb781c78196d12
Cal Jacobson <cj81499@users.noreply.github.com>
2025-12-02 20:48:49 +0100

    y2025d01 rework

--- src/aoc_cj/aoc2025/day01.py
+++ src/aoc_cj/aoc2025/day01.py
@@ -1,45 +1,41 @@
-from typing import Literal, TypeIs, override
+import dataclasses
+import functools
+import itertools
+import typing as t
+from collections.abc import Callable, Iterable
 
-type Direction = Literal["L", "R"]
+import more_itertools as mi
 
-
-def is_direction(d: str) -> TypeIs[Direction]:
-    return d in ("L", "R")
-
-
-class DialSolver:
-    def __init__(self) -> None:
-        self._dial_pos = 50
-        self._zero_count = 0
-
-    def _rotate(self, direction: Direction, distance: int) -> None:
-        move = -distance if direction == "L" else distance
-        self._dial_pos = (self._dial_pos + move) % 100
-        if self._dial_pos == 0:
-            self._zero_count += 1
-
-    def solve(self, rotations: list[str]) -> int:
-        for rotation in rotations:
-            direction = rotation[0]
-            assert is_direction(direction), f"rotation must start with 'L' or 'R'. Got '{direction}'"
-            distance = int(rotation[1:])
-            self._rotate(direction, distance)
-        return self._zero_count
+type Direction = t.Literal["L", "R"]
+_DIRECTIONS = frozenset[Direction](t.get_args(Direction.__value__))
 
 
-class DialSolver2(DialSolver):
-    @override
-    def _rotate(self, direction: Direction, distance: int) -> None:
-        for _ in range(distance):
-            super()._rotate(direction, 1)
+def is_direction(s: str) -> t.TypeIs[Direction]:
+    return s in _DIRECTIONS
 
 
-def part_1(txt: str) -> int:
-    return DialSolver().solve(txt.splitlines())
+@dataclasses.dataclass(frozen=True, kw_only=True, slots=True)
+class Rotation:
+    direction: Direction
+    distance: int
+
+    @classmethod
+    def parse(cls, s: str) -> t.Self:
+        direction = s[0]
+        assert is_direction(direction)
+        return cls(direction=direction, distance=int(s[1:]))
 
 
-def part_2(txt: str) -> int:
-    return DialSolver2().solve(txt.splitlines())
+simulate_dial = functools.partial(itertools.accumulate, func=lambda a, b: (a + b) % 100, initial=50)
+
+
+def solve(get_moves: Callable[[Rotation], Iterable[int]], txt: str) -> int:
+    moves = mi.flatten(get_moves(Rotation.parse(l)) for l in txt.splitlines())
+    return sum(pos == 0 for pos in simulate_dial(moves))
+
+
+part_1 = functools.partial(solve, lambda r: (-r.distance if r.direction == "L" else r.distance,))
+part_2 = functools.partial(solve, lambda r: itertools.repeat(-1 if r.direction == "L" else 1, r.distance))
 
 
 if __name__ == "__main__":
