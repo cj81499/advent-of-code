d519519b6af62fb3ea45a65c548c0285471e46b9
Cal Jacobson <cj81499@users.noreply.github.com>
2025-12-04 09:41:28 +0100

    rework y2023d10

--- src/aoc_cj/aoc2023/day10.py
+++ src/aoc_cj/aoc2023/day10.py
@@ -1,221 +1,126 @@
-import functools
-from collections import deque
-from collections.abc import Generator
+import abc
+import dataclasses
+import enum
+from collections.abc import Iterable
+from typing import Final, assert_never, override
 
+import frozendict
 import more_itertools as mi
 
-UP = -1j
-DOWN = +1j
-LEFT = -1
-RIGHT = +1
+from aoc_cj import util
 
 
-import dataclasses
+class ComplexEnum(complex, enum.ReprEnum):
+    """
+    Enum where members are also (and must be) `complex`s
+    """
 
 
-@dataclasses.dataclass(frozen=True, order=True)
-class GridPosition:
-    x: int
-    y: int
+class Direction(ComplexEnum):
+    NORTH = -1j
+    SOUTH = 1j
+    WEST = -1
+    EAST = 1
 
-    def _add(self, x: int, y: int) -> "GridPosition":
-        return GridPosition(self.x + x, self.y + y)
 
-    def up(self) -> "GridPosition":
-        return self._add(0, -1)
+ANIMAL: Final = "S"
 
-    def down(self) -> "GridPosition":
-        return self._add(0, 1)
 
-    def left(self) -> "GridPosition":
-        return self._add(-1, 0)
+class Pipe(enum.StrEnum):
+    NORTH_SOUTH = "|"
+    EAST_WEST = "-"
+    NORTH_EAST = "L"
+    NORTH_WEST = "J"
+    SOUTH_WEST = "7"
+    SOUTH_EAST = "F"
 
-    def right(self) -> "GridPosition":
-        return self._add(1, 0)
+    def connected_directions(self) -> tuple[Direction, Direction]:
+        match self:
+            case Pipe.NORTH_SOUTH:
+                return Direction.NORTH, Direction.SOUTH
+            case Pipe.EAST_WEST:
+                return Direction.EAST, Direction.WEST
+            case Pipe.NORTH_EAST:
+                return Direction.NORTH, Direction.EAST
+            case Pipe.NORTH_WEST:
+                return Direction.NORTH, Direction.WEST
+            case Pipe.SOUTH_WEST:
+                return Direction.SOUTH, Direction.WEST
+            case Pipe.SOUTH_EAST:
+                return Direction.SOUTH, Direction.EAST
+        assert_never(self)
 
-    def adj_4(self) -> tuple["GridPosition", "GridPosition", "GridPosition", "GridPosition"]:
-        return (self.up(), self.down(), self.left(), self.right())
 
-    def _top_left_corner(self) -> "TopLeftCorner":
-        return TopLeftCorner(self)
+@dataclasses.dataclass(frozen=True, kw_only=True, slots=True)
+class GridEntry(abc.ABC):
+    pos: complex
+    grid: Grid
 
-    def _top_right_corner(self) -> "TopLeftCorner":
-        return TopLeftCorner(self.right())
+    @abc.abstractmethod
+    def connections(self) -> Iterable[GridEntry]: ...
 
-    def _bottom_left_corner(self) -> "TopLeftCorner":
-        return TopLeftCorner(self.down())
 
-    def _bottom_right_corner(self) -> "TopLeftCorner":
-        return TopLeftCorner(self.down().right())
+@dataclasses.dataclass(frozen=True, kw_only=True, slots=True)
+class AnimalGridEntry(GridEntry):
+    @override
+    def connections(self) -> Iterable[GridEntry]:
+        return (ge for p in util.adj_4(self.pos) if (ge := self.grid.get(p)) is not None and self in ge.connections())
 
-    def corners(self) -> tuple["TopLeftCorner", "TopLeftCorner", "TopLeftCorner", "TopLeftCorner"]:
-        return (
-            self._top_left_corner(),
-            self._top_right_corner(),
-            self._bottom_left_corner(),
-            self._bottom_right_corner(),
+
+@dataclasses.dataclass(frozen=True, kw_only=True, slots=True)
+class PipeGridEntry(GridEntry):
+    pipe: Pipe
+
+    @override
+    def connections(self) -> Iterable[GridEntry]:
+        return (ge for d in self.pipe.connected_directions() if (ge := self.grid.get(self.pos + d)) is not None)
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True, slots=True)
+class Grid:
+    animal_pos: complex
+    pipes: frozendict.frozendict[complex, Pipe]
+
+    @staticmethod
+    def parse(txt: str) -> Grid:
+        grid = {complex(x, y): c for y, line in enumerate(txt.splitlines()) for x, c in enumerate(line) if c != "."}
+        return Grid(
+            animal_pos=mi.one(p for p, c in grid.items() if c == ANIMAL),
+            pipes=frozendict.frozendict((p, Pipe(c)) for p, c in grid.items() if c != ANIMAL),
         )
 
+    def get(self, pos: complex) -> PipeGridEntry | AnimalGridEntry | None:
+        if pos == self.animal_pos:
+            return self.get_animal()
+        pipe = self.pipes.get(pos)
+        if pipe is None:
+            return None
+        return PipeGridEntry(pos=pos, grid=self, pipe=pipe)
 
-import dataclasses
-
-
-@dataclasses.dataclass(frozen=True, order=True)
-class TopLeftCorner:
-    pos: GridPosition
-
-    def up(self) -> "TopLeftCorner":
-        return TopLeftCorner(self.pos.up())
-
-    def down(self) -> "TopLeftCorner":
-        return TopLeftCorner(self.pos.down())
-
-    def left(self) -> "TopLeftCorner":
-        return TopLeftCorner(self.pos.left())
-
-    def right(self) -> "TopLeftCorner":
-        return TopLeftCorner(self.pos.right())
-
-
-import dataclasses
-
-
-@dataclasses.dataclass(frozen=True, order=True)
-class Grid:
-    contents: dict[GridPosition, str]
-
-    def start_pos(self) -> GridPosition:
-        return mi.one(p for p, c in self.contents.items() if c == "S")
-
-    def connections(self, p: GridPosition) -> Generator[GridPosition]:
-        pipe_type = self.contents.get(p, ".")
-
-        if pipe_type == ".":
-            return
-
-        if pipe_type == "|":
-            yield p.up()
-            yield p.down()
-            return
-
-        if pipe_type == "-":
-            yield p.left()
-            yield p.right()
-            return
-
-        if pipe_type == "L":
-            yield p.up()
-            yield p.right()
-            return
-
-        if pipe_type == "J":
-            yield p.up()
-            yield p.left()
-            return
-
-        if pipe_type == "7":
-            yield p.down()
-            yield p.left()
-            return
-
-        if pipe_type == "F":
-            yield p.down()
-            yield p.right()
-            return
-
-        if pipe_type == "S":
-            for adj_p in p.adj_4():
-                if p in self.connections(adj_p):
-                    yield adj_p
-            return
-
-        assert False, f"unexpected value in pipe: '{pipe_type}'"
-
-    @functools.cached_property
-    def _max_x(self) -> int:
-        return max(p.x for p in self.contents)
-
-    @functools.cached_property
-    def _max_y(self) -> int:
-        return max(p.y for p in self.contents)
-
-    def corner_in_bounds(self, corner: TopLeftCorner) -> bool:
-        return (0 <= corner.pos.x <= self._max_x + 1) and (0 <= corner.pos.y <= self._max_y + 1)
-
-    @functools.cached_property
-    def loop(self) -> dict[GridPosition, int]:
-        loop_distances: dict[GridPosition, int] = {}
-        to_explore = deque([(self.start_pos(), 0)])
-        while to_explore:
-            p, distance = to_explore.popleft()
-            if p in loop_distances:
-                continue
-            loop_distances[p] = distance
-            for adj_p in self.connections(p):
-                if adj_p not in loop_distances:
-                    to_explore.append((adj_p, distance + 1))
-        return loop_distances
-
-    def can_flood(self, from_corner: TopLeftCorner, direction: complex) -> bool:
-        # find grid positions to check
-        p1 = from_corner.pos
-        if direction != DOWN:
-            p1 = p1.up()
-        if direction != RIGHT:
-            p1 = p1.left()
-
-        p2 = from_corner.pos
-        if direction == UP:
-            p2 = p2.up()
-        if direction == LEFT:
-            p2 = p2.left()
-
-        # if either position is not in the loop, it can be flooded
-        if p1 not in self.loop or p2 not in self.loop:
-            return True
-        # if the both positions are in the loop, it can be flooded if they're not connected
-        return p1 not in self.connections(p2)
+    def get_animal(self) -> AnimalGridEntry:
+        return AnimalGridEntry(pos=self.animal_pos, grid=self)
 
 
 def part_1(txt: str) -> int:
-    grid = Grid({GridPosition(x, y): c for y, line in enumerate(txt.splitlines()) for x, c in enumerate(line)})
-    return max(grid.loop.values())
+    g = Grid.parse(txt)
+
+    # starting from the animal, traverse along the pipe in both directions until
+    # the two "pointers" meet. at that point, we've found the furthest point from
+    # the start. there will always be exactly one furthest point, so we don't
+    # need to worry about the pointers "crossing" without seeing one another.
+    animal = g.get_animal()
+    pipe_loop: set[GridEntry] = {animal}
+    exploring = tuple(animal.connections())
+    steps = 1
+    while not mi.all_equal(exploring):
+        pipe_loop.update(exploring)
+        exploring = tuple(mi.one(adj for adj in entry.connections() if adj not in pipe_loop) for entry in exploring)
+        steps += 1
+    return steps
 
 
 def part_2(txt: str) -> int:
-    grid = Grid({GridPosition(x, y): c for y, line in enumerate(txt.splitlines()) for x, c in enumerate(line)})
-
-    # to find tiles enclosed by the loop, we will flood the corners of each grid position
-    # (not the grid positions themselves to allow for flooding to move between disconnected pipes)
-    # at the end, grid positions with no flooded corners are enclosed within the loop
-
-    flood_reachable = set[TopLeftCorner]()
-    # starting from top left corner of (0, 0), we will "flood" the corners of the grid.
-    to_explore = deque([TopLeftCorner(GridPosition(0, 0))])
-    while to_explore:
-        corner = to_explore.popleft()
-
-        # if corner is out of bounds, skip it
-        if not grid.corner_in_bounds(corner):
-            continue
-
-        # if we've already flooded corner, skip it
-        if corner in flood_reachable:
-            continue
-        flood_reachable.add(corner)
-
-        if grid.can_flood(corner, UP):  # flood up
-            to_explore.append(corner.up())
-        if grid.can_flood(corner, DOWN):  # flood down
-            to_explore.append(corner.down())
-        if grid.can_flood(corner, LEFT):  # flood left
-            to_explore.append(corner.left())
-        if grid.can_flood(corner, RIGHT):  # flood right
-            to_explore.append(corner.right())
-
-    # grid spaces that have no flooded corners are enclosed by the loop
-    # count grid positions where no corner was reached during the flooding process
-    return sum(1 for p in grid.contents if not any(p in flood_reachable for p in p.corners()))
+    raise NotImplementedError
 
 
 if __name__ == "__main__":
