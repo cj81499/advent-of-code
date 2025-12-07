afba8392724d8d61324ae11af00b90a8f0bd8b85
Cal Jacobson <cj81499@users.noreply.github.com>
2025-12-07 16:51:01 -0500

    cleanup y2025d07

--- src/aoc_cj/aoc2025/day07.py
+++ src/aoc_cj/aoc2025/day07.py
@@ -12,21 +12,15 @@ class GridEntry(enum.StrEnum):
 
 
 def part_1(txt: str) -> int:
-    g = {complex(x, y): GridEntry(c) for y, line in enumerate(txt.splitlines()) for x, c in enumerate(line) if c != "."}
-    start = mi.one(p for p, ge in g.items() if ge == GridEntry.START)
+    rows = tuple(tuple(map(GridEntry, line)) for line in txt.splitlines())
+    start = mi.one(x for x, ge in enumerate(rows[0]) if ge == GridEntry.START)
     beams = {start}
     split_count = 0
-    for _ in range(10_000):  # HACK: 10,000 if probably enough...
-        new_beams = set[complex]()
-        for b in beams:
-            below = b + 1j
-            if g.get(below) == GridEntry.SPLITTER:
-                split_count += 1
-                new_beams.add(below - 1)
-                new_beams.add(below + 1)
-            else:
-                new_beams.add(below)
-        beams = new_beams
+    for row in rows:
+        moving_to_empty, moving_to_splitter = mi.partition(lambda b: row[b] == GridEntry.SPLITTER, beams)
+        moving_to_splitter = mi.countable(moving_to_splitter)
+        beams = {*moving_to_empty, *mi.flatten((b - 1, b + 1) for b in moving_to_splitter)}
+        split_count += moving_to_splitter.items_seen
     return split_count
 
 
@@ -37,7 +31,6 @@ def part_2(txt: str) -> int:
 
     @functools.cache
     def _helper(p: complex) -> int:
-        print(p)
         if p.imag > max_y:
             return 1
         ge = g.get(p)
